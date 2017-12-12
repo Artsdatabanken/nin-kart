7e222368e03a4d6aec766e8af108d727ae1eed4a
amned <amund.nedland@norconsult.com>
2017-12-12 13:01:33 +0100

    add react routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
 import NatureAreaDetailContainer from './Naturområdedetaljer/NatureAreaDetailContainer'
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import FilterTreeContainer from './FilterTree/FilterTreeContainer'
@@ -10,17 +12,24 @@ class App extends Component {
     return (
       <div className="App">
           <MuiThemeProvider>
-              <div>
-                  <FilterTreeContainer/>
-                  <NatureAreaDetailContainer
-                    natureAreaId={'3b06e6f4-402b-4844-87bc-9a7c7a872cb2'}/>
-                  <StepSliderContainer/>
-                  <RangeSliderContainer />
-              </div>
+              <BrowserRouter>
+                  <Switch>
+                      <Route path="/tree" exact component={FilterTreeContainer} />
+                      <Route path="/details" exact
+                             render={() => <NatureAreaDetailContainer
+                                 natureAreaId={'3b06e6f4-402b-4844-87bc-9a7c7a872cb2'}/>} />
+                      <Route path="/range" exact
+                             render={() => <div><StepSliderContainer/>
+                                 <RangeSliderContainer /></div>} />
+                      <Route component={RedirectToDefault} />
+                  </Switch>
+              </BrowserRouter>
           </MuiThemeProvider>
       </div>
     );
   }
 }
+
+const RedirectToDefault = () => <Redirect from="/" to="/tree" />;
 
 export default App;
