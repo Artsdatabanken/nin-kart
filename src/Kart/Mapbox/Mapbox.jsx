ea8b03cfe106be6d2d6741958e90b6778ec99daa
Bjørn Reppen <bjreppen@gmail.com>
2017-12-21 14:18:57 +0100

    Enable map navigation

--- src/Kart/Mapbox/Mapbox.jsx
+++ src/Kart/Mapbox/Mapbox.jsx
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import ReactMapGL from 'react-map-gl';
 
 class Mapbox extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: 63.459,
+      longitude: 10.921,
+      zoom: 3.5
+    }
+  }
+
+  _onViewportChange = viewport =>
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+  })
+
+  onClick = point => {
+    alert(point.lngLat)
+  }
+
     render() {
-        const {latitude, longitude, zoom} = this.props
         return <ReactMapGL
-        width={window.innerWidth}
-        height={window.innerHeight}
-        latitude={latitude}
-        longitude={longitude}
-        zoom={zoom}
-        onViewportChange={(viewport) => {
-          const {width, height, latitude, longitude, zoom} = viewport;
-          // Optionally call `setState` and use the state to update the map.
-        }}
+        {...this.state.viewport}
+        onClick={this.onClick}
+        onViewportChange={viewport => this._onViewportChange(viewport)}
         mapboxApiAccessToken="pk.eyJ1IjoiYmpyZXBwZW4iLCJhIjoiY2ltZGFkMW11MDAwdnZpbHVsamhsZzB1dSJ9.oZBI8rZR8YSsXoyIM0vLYg"
       />
     }
