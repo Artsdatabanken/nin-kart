667716e150528ee204798d4fe0b7dd3719fcb049
Bjørn Reppen <bjreppen@gmail.com>
2017-12-22 01:23:28 +0100

    Legger til Mapbox default kart (story) (#8)
    * Legger til Mapbox default kart (story)
    
    * Parametriser lat, lon, zoom
    
    * Enable map navigation

---
+++ src/Kart/Mapbox/Mapbox.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ReactMapGL from 'react-map-gl';
+
+class Mapbox extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: 63.459,
+      longitude: 10.921,
+      zoom: 3.5
+    }
+  }
+
+  _onViewportChange = viewport =>
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+  })
+
+  onClick = point => {
+    alert(point.lngLat)
+  }
+
+    render() {
+        return <ReactMapGL
+        {...this.state.viewport}
+        onClick={this.onClick}
+        onViewportChange={viewport => this._onViewportChange(viewport)}
+        mapboxApiAccessToken="pk.eyJ1IjoiYmpyZXBwZW4iLCJhIjoiY2ltZGFkMW11MDAwdnZpbHVsamhsZzB1dSJ9.oZBI8rZR8YSsXoyIM0vLYg"
+      />
+    }
+}
+
+export default Mapbox;
