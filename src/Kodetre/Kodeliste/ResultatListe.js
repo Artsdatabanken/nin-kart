63c4e9ace815ae046c88c933cb7642e196767188
Bjørn Reppen <bjreppen@gmail.com>
2018-01-28 20:48:37 +0100

    Klikk på element i søkersultat for å navigere til elementet

--- src/Kodetre/Kodeliste/ResultatListe.js
+++ src/Kodetre/Kodeliste/ResultatListe.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
 import { List, ListItem, Avatar } from 'material-ui'
 
 class ResultatListe extends Component {
   render() {
     return (
       <List style={{ overflow: 'auto', maxHeight: 600 }}>
-        <Route
-          render={({ history }) =>
-            this.props.searchResults.map(item => (
-              <ListItem
-                style={{ width: 500, pointer: 'hand' }}
-                onClick={() => {
-                  history.push('/' + item.code)
-                  this.setState({ searchResults: null })
-                }}
-                key={item.code}
-                primaryText={item.name}
-                secondaryText={item.code}
-                leftAvatar={
-                  <Avatar src="https://www.artsdatabanken.no/Media/F1698?mode=480x480" />
-                }
-              />
-            ))
-          }
-        />
+        {this.props.searchResults.map(item => (
+          <ListItem
+            style={{ width: 500, pointer: 'hand' }}
+            onClick={() => this.props.onClick(item.code)}
+            key={item.code}
+            primaryText={item.name}
+            secondaryText={item.code}
+            leftAvatar={
+              <Avatar src="https://www.artsdatabanken.no/Media/F1698?mode=480x480" />
+            }
+          />
+        ))}
       </List>
     )
   }
