b292785d70e298f144306a5b6e0e3d4f412dd6b3
jarle pedersen <jarle.pedersen@norconsult.com>
2018-02-05 17:22:59 +0100

    feed the coverAlls

--- src/Naturområdedetaljer/PunktinformasjonContainer.stories.js
+++ src/Naturområdedetaljer/PunktinformasjonContainer.stories.js
@@ -2,6 +2,17 @@ import PunktinformasjonContainer from './PunktinformasjonContainer'
 
 var punktinformasjonContainer = new PunktinformasjonContainer()
 
-test('fixStedsnavn', () => {
+test('fixStedsnavn-NullTest', () => {
   expect(punktinformasjonContainer.fixStedsnavn({ result: 'nada' })).toBe(null)
+})
+
+test('fixStedsnavn', () => {
+  expect(
+    Object.keys(
+      punktinformasjonContainer.fixStedsnavn({
+        placename: 'huttiheita',
+        stedsnummer: 42,
+      })
+    )[0]
+  ).toBe('GEO_SN-42')
 })
