3d56077b9baad96586ab13c26e89a11ded8f78d5
Bjørn Reppen <bjreppen@gmail.com>
2018-02-06 09:14:29 +0100

    Coverbilder på nin-typer (#66)
    * Coverbilder på nin-typer
    
    * Improve coverage
    
    * Fix broken build

--- src/Kodetre/Kodeliste/ResultatListe.js
+++ src/Kodetre/Kodeliste/ResultatListe.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react'
-import { Paper, List, ListItem, Avatar } from 'material-ui'
+import { List, ListItem, Avatar } from 'material-ui'
 
 class ResultatListe extends Component {
   render() {
-    if (this.props.searchResults.length <= 0) return null
     return (
-      <Paper>
-        <List style={{ overflow: 'auto', maxHeight: 600 }}>
-          {this.props.searchResults.map(item => (
-            <ListItem
-              style={{ width: 500, pointer: 'hand' }}
-              onClick={() => this.props.onClick(item.code)}
-              key={item.code}
-              primaryText={item.name}
-              secondaryText={item.code}
-              leftAvatar={
-                <Avatar src="https://www.artsdatabanken.no/Media/F1698?mode=480x480" />
-              }
-            />
-          ))}
-        </List>
-      </Paper>
+      <List style={{ overflow: 'auto', maxHeight: 600 }}>
+        {this.props.searchResults.map(item => (
+          <ListItem
+            style={{ width: 500, pointer: 'hand' }}
+            onClick={() => this.props.onClick(item.code)}
+            key={item.code}
+            primaryText={item.name}
+            secondaryText={item.code}
+            leftAvatar={
+              <Avatar src="https://www.artsdatabanken.no/Media/F1698?mode=480x480" />
+            }
+          />
+        ))}
+      </List>
     )
   }
 }
---
+++ src/VenstreVinduContainer.stories.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { muiTheme } from 'storybook-addon-material-ui'
+import { MemoryRouter } from 'react-router-dom'
+import VenstreVinduContainer from './VenstreVinduContainer'
+
+storiesOf('VenstreVindu', module)
+  .addDecorator(muiTheme())
+  .add('Rot', () => {
+    return (
+      <div style={{ padding: 8 }}>
+        <MemoryRouter>
+          <VenstreVinduContainer />
+        </MemoryRouter>
+      </div>
+    )
+  })
