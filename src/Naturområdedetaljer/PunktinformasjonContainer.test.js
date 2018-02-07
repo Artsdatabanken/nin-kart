1640b020ee205262f97be370d0201c8476e1bf0e
jarle pedersen <jarle.pedersen@norconsult.com>
2018-02-07 15:57:28 +0100

    test for createNatureAreaPointInfo

--- src/Naturområdedetaljer/PunktinformasjonContainer.test.js
+++ src/Naturområdedetaljer/PunktinformasjonContainer.test.js
@@ -16,3 +16,26 @@ test('fixStedsnavn', () => {
     )[0]
   ).toBe('GEO_SN-42')
 })
+
+var createNatureAreaPointInfoName = 'NA_T32-C-3'
+var createNatureAreaPointInfoValue = 'intermediær eng med mindre hevdpreg'
+
+var naturAreaExpected = {
+  name: createNatureAreaPointInfoName,
+  value: createNatureAreaPointInfoValue,
+  logo:
+    'https://pbs.twimg.com/profile_images/378800000067455227/3d053db6b9593d47a02ced7709846522_400x400.png',
+  homepage: 'http://www.miljodirektoratet.no/',
+  dataorigin: 'MDIR',
+  article: 'https://www.artsdatabanken.no/Pages/222921',
+}
+
+test('createNatureAreaPointInfo'),
+  () => {
+    expect(
+      punktinformasjonContainer.createNatureAreaPointInfo(
+        createNatureAreaPointInfoName,
+        createNatureAreaPointInfoValue
+      )
+    ).toBe(naturAreaExpected)
+  }
