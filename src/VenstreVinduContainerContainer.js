5b98360266eaa765c1b285d0581ff7c911852381
amundn <amundn@users.noreply.github.com>
2018-02-09 13:01:35 +0100

    - fjernet kontrollpanel (#92)
    - henter filter fra firebase hvis det finnes

--- src/VenstreVinduContainerContainer.js
+++ src/VenstreVinduContainerContainer.js
@@ -64,16 +64,10 @@ class VenstreVinduContainerContainer extends React.Component {
           <VenstreVinduContainer
             filterCode={'filterCodes'}
             mapbounds={this.props.mapbounds}
-
             onToggleMainDrawer={this.props.onToggleMainDrawer}
             onCheckChange={this.handleCheckChange}
             isSelected={this.isSelected}
             filter={this.state.filterCodes}
-            onVisibilityChange={this.props.onVisibilityChange}
-            onColorChange={this.props.onColorChange}
-            categories={this.props.categories}
-            visibility={this.props.visibility}
-            color={this.props.color}
           />
         )}
       />
