b75a7bf76c4886d8b336e87376d1e7b0876df8e6
Bjørn Reppen <bjreppen@gmail.com>
2018-02-27 10:59:13 +0100

    Sorter på kode hvis prioritering mangler

--- src/Kodetre/Kodeliste/Kodeliste.js
+++ src/Kodetre/Kodeliste/Kodeliste.js
@@ -2,6 +2,15 @@ import React from 'react'
 import Kodelisteelement from './Kodelisteelement'
 import { Subheader } from 'material-ui'
 
+function sort(metadata) {
+  return Object.keys(metadata).sort((a, b) => {
+    const ma = metadata[a]
+    const mb = metadata[b]
+    if (ma.sortering && mb.sortering) return ma.sortering > mb.sortering
+    return ma.kode > mb.kode
+  })
+}
+
 const Kodeliste = ({
   title,
   apidata,
@@ -16,36 +25,34 @@ const Kodeliste = ({
   if (!metadata) return null
   return (
     <React.Fragment>
-      {Object.keys(metadata)
-        .sort((a, b) => metadata[a].sortering > metadata[b].sortering)
-        .map(item => {
-          const apibarn = apidata
-            ? apidata[
-                apidata
-                  .map(apiItem => {
-                    return apiItem.kode
-                  })
-                  .indexOf(item)
-              ] || {}
-            : {}
-          const metabarnet = metadata[item] || {}
-          const kode = item.toString()
-          return (
-            <Kodelisteelement
-              kode={kode}
-              key={kode}
-              subkode={item}
-              {...apibarn}
-              meta={metabarnet}
-              erEkspandert={kode === ekspandertKode}
-              onGoToCode={onGoToCode}
-              onMouseEnter={onMouseEnter}
-              onMouseLeave={onMouseLeave}
-              onUpdateLayerProp={onUpdateLayerProp}
-              onShowColorpicker={() => onShowColorpicker(kode)}
-            />
-          )
-        })}
+      {sort(metadata).map(item => {
+        const apibarn = apidata
+          ? apidata[
+              apidata
+                .map(apiItem => {
+                  return apiItem.kode
+                })
+                .indexOf(item)
+            ] || {}
+          : {}
+        const metabarnet = metadata[item] || {}
+        const kode = item.toString()
+        return (
+          <Kodelisteelement
+            kode={kode}
+            key={kode}
+            subkode={item}
+            {...apibarn}
+            meta={metabarnet}
+            erEkspandert={kode === ekspandertKode}
+            onGoToCode={onGoToCode}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            onUpdateLayerProp={onUpdateLayerProp}
+            onShowColorpicker={() => onShowColorpicker(kode)}
+          />
+        )
+      })}
     </React.Fragment>
   )
 }
