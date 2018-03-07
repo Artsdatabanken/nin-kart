4bfc2f6d5a65958658ae8cc66cf92f2c9810cb09
Bjørn Reppen <bjreppen@gmail.com>
2018-03-07 14:53:17 +0100

    Søkeboks kosmetikk

--- src/SearchBox/SearchBox.js
+++ src/SearchBox/SearchBox.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import { TextField } from 'material-ui'
-import getNext from '../componentid'
+//import getNext from '../componentid'
 import backend from '../backend'
 import PropTypes from 'prop-types'
 import rename from '../rename'
 
 export default class SearchBox extends Component {
   queryNumber = 0
+  state = {}
 
   handleChange = (e, q) => {
     console.log('handleChange', q)
+    this.setState({ query: q })
     this.queryNumber++
     this.props.onSearchResults(null)
     const currentQuery = this.queryNumber
@@ -23,31 +25,38 @@ export default class SearchBox extends Component {
 
   handleFocus = e => {
     console.log('handleFocus', this.props.title)
-    this.handleChange(e, this.props.title)
+    this.handleChange(e, this.state.query)
   }
 
   onKeyDown = e => {
     if (e.keyCode === 27) {
-      this.props.onBlur()
+      this.handleChange(e, '')
+      this.textField.blur()
       e.stopPropagation()
     }
   }
 
+  componentWillReceiveProps(nextProps, props) {
+    console.log(nextProps, props)
+    if (nextProps.query !== this.props.query)
+      this.setState({ query: nextProps.query || '' })
+  }
+
   render() {
-    console.log('q', this.props.query)
+    const query = this.state.query
     return (
       <TextField
-        onBlur={this.props.onBlur}
+        ref={tf => (this.textField = tf)}
         onKeyDown={this.onKeyDown}
-        //        id={getNext()}
-        value={this.props.query}
-        //        hintText={this.props.title ? this.props.title : 'Økologisk grunnkart'}
+        //id={getNext()}
+        value={query}
+        hintText={!query && 'Økologisk grunnkart'}
         onFocus={this.handleFocus}
         onChange={this.handleChange}
         fullWidth={true}
-        //        autoFocus
         underlineShow={false}
-        style={{ height: 48, cursor: 'text' }}
+        inputStyle={{ fontSize: '15px' }}
+        hintStyle={{ fontSize: '15px' }}
       />
     )
   }
--- src/TopBar/TopBar.js
+++ src/TopBar/TopBar.js
@@ -35,55 +35,53 @@ class TopBar extends Component {
 
   render() {
     return (
-      <div>
-        <AppBar
-          style={{ backgroundColor: '#ffffff' }}
-          title={
-            <SearchBox
-              query={this.props.query}
-              onSearchResults={this.props.onSearchResults}
-            />
-          }
-          iconElementLeft={
-            this.props.isAtRoot ? (
-              <IconButton onClick={this.props.onToggleMainDrawer}>
-                <Hamburger color="#5f5f5f" />
-              </IconButton>
-            ) : (
-              <IconButton onClick={this.props.onGoBack}>
-                <NavigationBack color="#5f5f5f" />
-              </IconButton>
-            )
-          }
-          iconElementRight={
-            <React.Fragment>
-              <IconButton onClick={this.handleSearchButtonClick}>
-                <Search color="#b4b4b4" />
-              </IconButton>
-              {!this.props.isAtRoot && (
-                <React.Fragment>
-                  <div
-                    style={{
-                      position: 'absolute',
-                      height: 25,
-                      borderLeft: '1px solid #ddd',
-                      marginTop: 12,
-                      top: 0,
-                      right: 55,
-                    }}
-                  />
-                  <IconButton
-                    onClick={this.props.onExitToRoot}
-                    style={{ position: 'relative' }}
-                  >
-                    <CloseIcon color="#b4b4b4" />
-                  </IconButton>
-                </React.Fragment>
-              )}
-            </React.Fragment>
-          }
-        />
-      </div>
+      <AppBar
+        style={{ backgroundColor: '#ffffff' }}
+        title={
+          <SearchBox
+            query={this.props.query}
+            onSearchResults={this.props.onSearchResults}
+          />
+        }
+        iconElementLeft={
+          this.props.isAtRoot ? (
+            <IconButton onClick={this.props.onToggleMainDrawer}>
+              <Hamburger color="#5f5f5f" />
+            </IconButton>
+          ) : (
+            <IconButton onClick={this.props.onGoBack}>
+              <NavigationBack color="#5f5f5f" />
+            </IconButton>
+          )
+        }
+        iconElementRight={
+          <React.Fragment>
+            <IconButton onClick={this.handleSearchButtonClick}>
+              <Search color="#b4b4b4" />
+            </IconButton>
+            {!this.props.isAtRoot && (
+              <React.Fragment>
+                <div
+                  style={{
+                    position: 'absolute',
+                    height: 25,
+                    borderLeft: '1px solid #ddd',
+                    marginTop: 12,
+                    top: 0,
+                    right: 55,
+                  }}
+                />
+                <IconButton
+                  onClick={this.props.onExitToRoot}
+                  style={{ position: 'relative' }}
+                >
+                  <CloseIcon color="#b4b4b4" />
+                </IconButton>
+              </React.Fragment>
+            )}
+          </React.Fragment>
+        }
+      />
     )
   }
 }
