8100f004ad0fbdf69b8e78a6602150dbe5dd1103
Bjørn Reppen <bjreppen@gmail.com>
2018-03-08 08:39:25 +0100

    Increase test coverage

--- src/Kodetre/Kodeliste/ResultatListe.stories.js
+++ src/Kodetre/Kodeliste/ResultatListe.stories.js
@@ -3,12 +3,12 @@ import { storiesOf } from '@storybook/react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Resultatliste from './ResultatListe'
 
-const results = [{ kode: 'KA', tittel: { nb: 'Kalk' } }]
+const results = [{ kode: 'KA', navn: 'Kalk' }]
 
 storiesOf('Resultatliste', module).add('default', () => {
   return (
     <MuiThemeProvider>
-      <Resultatliste searchResults={results} />
+      <Resultatliste searchResults={results} query="alk" />
     </MuiThemeProvider>
   )
 })
