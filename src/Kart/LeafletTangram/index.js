bfbf840cfac5a7be1ecee5722d055934ee0c424a
Bjørn Reppen <bjreppen@gmail.com>
2018-03-11 11:03:09 +0100

    Import Tangram

--- src/Kart/LeafletTangram/index.js
+++ src/Kart/LeafletTangram/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import L from 'leaflet'
 import { createLeafletLayer } from './scene'
@@ -98,3 +99,35 @@ class LeafletTangram extends React.Component {
 }
 
 export default LeafletTangram
+||||||| merged common ancestors
+=======
+import React from 'react'
+import L from 'leaflet'
+import Tangram from 'tangram'
+
+class LeafletTangram extends React.Component {
+  componentDidMount() {
+    const map = L.map(this.mapEl)
+    const layer = Tangram.leafletLayer({
+      scene: 'https://mapzen.com/carto/bubble-wrap-style/bubble-wrap.yaml',
+      attribution:
+        '<a href="https://mapzen.com/tangram">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/">Mapzen</a>',
+    })
+    layer.addTo(map)
+
+    map.setView([40.70531887544228, -74.00976419448853], 15)
+  }
+
+  render() {
+    return (
+      <div
+        ref={ref => {
+          this.mapEl = ref
+        }}
+      />
+    )
+  }
+}
+
+export default LeafletTangram
+>>>>>>> Import Tangram
--- src/Kart/Mapbox/style-lookup.js
+++ src/Kart/Mapbox/style-lookup.js
@@ -5,7 +5,43 @@ function hentLag(map, kode) {
   let layer = map.getLayer(kode)
   if (layer) return layer
 
-  if (kode.endsWith('RASTER')) {
+  const fylkeNr = 1 //getFylke(kode)
+
+  if (fylkeNr) {
+    const filter =
+      fylkeNr === 'ALL' ? ['!=', 'AO', '-1'] : ['in', 'AO', '', fylkeNr]
+    return {
+      id: kode,
+      type: 'fill',
+      custom: true,
+      source: 'composite',
+      'source-layer': 'FY',
+      filter: filter,
+      layout: {},
+      paint: {
+        'fill-outline-color': {
+          base: 1,
+          stops: [
+            [0, 'hsla(0, 0%, 0%, 70%)'],
+            [7, 'hsla(0, 0%, 0%, 70%)'],
+            [10, 'hsla(0, 0%, 0%, 0%)'],
+          ],
+        },
+        'fill-color': {
+          base: 1,
+          stops: [
+            [0, 'hsla(0, 0%, 100%, 20%)'],
+            [7, 'hsla(0, 0%, 100%, 20%)'],
+            [10, 'hsla(0, 0%, 0%, 0%)'],
+          ],
+        },
+      },
+    }
+  }
+
+  let kommuneMatch = kode.match(/AO_([0-9][0-9])-(.*)/)
+  if (kommuneMatch && kommuneMatch.length === 3) {
+    let kommuneNr = '' + (kommuneMatch[1] + kommuneMatch[2])
     return {
       id: kode,
       type: 'raster',
