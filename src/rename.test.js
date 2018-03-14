d5776f86a0a9d334c083aa1bc679a66690ccb3aa
amundn <amundn@users.noreply.github.com>
2018-03-14 14:36:11 +0100

    * styling for søkeresultat (#179)
    * * styling for søkeresultat
    * tilpasset endret søke-api
    * * oppdatert rename-test

--- src/rename.test.js
+++ src/rename.test.js
@@ -1,5 +1,30 @@
 import rename from './rename'
 
+const searchResultInput = [
+  {
+    kode: 'NA_F1-3',
+    navn: {
+      nb: 'kalkfattige klare elvevannmasser i stryk, fossestryk og fosser',
+    },
+    forelder: { kode: 'NA_F1', navn: { nb: 'Elvevannmasser' } },
+  },
+]
+const searchResultOutput = [
+  {
+    kode: 'NA_F1-3',
+    navn: {
+      nb: 'Kalkfattige klare elvevannmasser i stryk, fossestryk og fosser',
+    },
+    forelder: { kode: 'NA_F1', navn: { nb: 'Elvevannmasser' } },
+  },
+]
+
 test('foreslåtte renames', () => {
   expect(rename('ok')).toBe('ok')
+})
+
+test('rename from object array', () => {
+  expect(JSON.stringify(rename(searchResultInput))).toBe(
+    JSON.stringify(searchResultOutput)
+  )
 })
