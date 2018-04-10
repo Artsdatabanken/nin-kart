474b7ec5e3bd74b7ad8fe9ee036d308bbc7ebe06
amned <amund.nedland@norconsult.com>
2018-04-10 09:18:29 +0200

    oppdater kodevindu.stories for å teste relasjon-listene

--- src/Kodetre/Kodeliste/KodeVindu.stories.js
+++ src/Kodetre/Kodeliste/KodeVindu.stories.js
@@ -95,12 +95,171 @@ const meta = {
   tittel: { nb: 'Marine vannmasser' },
 }
 
+const api2 = {
+  kode: 'NA_M8',
+  navn: 'Helofytt-saltvannssump',
+  antallArter: 1,
+  antallNaturomrader: 381,
+  areal: 14444776.381085396,
+  forelder: { kode: 'NA_M', navn: 'Saltvannsbunnsystemer' },
+}
+const meta2 = {
+  bbox: [[4.955, 58.016], [29.999, 70.526]],
+  definisjonsgrunnlag: {
+    kode: 'NA_HT-DGA',
+    tittel: { nb: 'normal, strukturerende artsgruppe' },
+  },
+  farge: '#e6f598',
+  infoUrl: 'https://www.artsdatabanken.no/NiN2.0/M8',
+  ingress:
+    'Helofytt-saltvannssump omfatter tette bestander av makrohelofytter, det vil si storvokste sumpplanter med røttene i sublitoral bunn (som ikke tørrlegges ved lavvann), i vannstrandbeltet eller noe opp i landstrandbeltet.\n',
+  kode: 'NA_M8',
+  kunnskap: {
+    generelt: { kode: 'NA_HT-KG2', verdi: 2 },
+    inndeling: { kode: 'NA_HT-KG-GI2', verdi: 2 },
+  },
+  lkm: { u: ['MI_SA', 'MI_IO', 'MI_TV'] },
+  nin1: 'S7 [10, 12]',
+  nivå: 'hovedtype',
+  overordnet: [
+    { kode: 'NA_M', sti: 'NA/M', tittel: { nb: 'Saltvannsbunnsystemer' } },
+    { kode: 'NA', sti: 'NA', tittel: { nb: 'Natursystem' } },
+    { kode: '~', sti: '', tittel: { nb: 'Økologisk grunnkart' } },
+  ],
+  prosedyrekategori: {
+    kode: 'NA_HT-PK2',
+    tittel: {
+      nb:
+        'Med variasjon i artssammensetning betinget av strukturerende artsgruppe',
+    },
+  },
+  relasjon: {
+    dominerende_mengdeart: {
+      AR_100132: {
+        kode: 'AR_100132',
+        tittel: { la: 'Phragmites australis', nb: 'Takrør' },
+        variabel: 'dominerende mengdeart',
+      },
+    },
+    kjennetegnende_tyngdepunktart: {
+      AR_99475: {
+        kode: 'AR_99475',
+        tittel: { la: 'Iris pseudacorus', nb: 'Sverdlilje' },
+        variabel: 'kjennetegnende tyngdepunktart',
+      },
+      AR_99603: {
+        kode: 'AR_99603',
+        tittel: { la: 'Bolboschoenus maritimus', nb: 'Havsivaks' },
+        variabel: 'kjennetegnende tyngdepunktart',
+      },
+      AR_99698: {
+        kode: 'AR_99698',
+        tittel: { la: 'Carex paleacea', nb: 'Havstarr' },
+        variabel: 'kjennetegnende tyngdepunktart',
+      },
+      AR_99737: {
+        kode: 'AR_99737',
+        tittel: { la: 'Carex ×vacillans', nb: 'Saltstarr' },
+        variabel: 'kjennetegnende tyngdepunktart',
+      },
+      AR_99790: {
+        kode: 'AR_99790',
+        tittel: { la: 'Schoenoplectus tabernaemontani', nb: 'Pollsivaks' },
+        variabel: 'kjennetegnende tyngdepunktart',
+      },
+    },
+    mengdeart: {
+      AR_99603: {
+        kode: 'AR_99603',
+        tittel: { la: 'Bolboschoenus maritimus', nb: 'Havsivaks' },
+        variabel: '',
+      },
+    },
+    tyngdepunktart: {
+      AR_99677: {
+        kode: 'AR_99677',
+        tittel: { la: 'Carex mackenziei', nb: 'Pølstarr' },
+        variabel: '',
+      },
+    },
+    vanlig_art: {
+      AR_100058: {
+        kode: 'AR_100058',
+        tittel: { la: 'Glyceria fluitans', nb: 'Mannasøtgras' },
+        variabel: 'vanlig art',
+      },
+      AR_100061: {
+        kode: 'AR_100061',
+        tittel: { la: 'Glyceria maxima', nb: 'Kjempesøtgras' },
+        variabel: 'vanlig art',
+      },
+      AR_100132: {
+        kode: 'AR_100132',
+        tittel: { la: 'Phragmites australis', nb: 'Takrør' },
+        variabel: 'vanlig art',
+      },
+      AR_101845: {
+        kode: 'AR_101845',
+        tittel: { la: 'Lysimachia thyrsiflora', nb: 'Gulldusk' },
+        variabel: 'vanlig art',
+      },
+      AR_101846: {
+        kode: 'AR_101846',
+        tittel: { la: 'Lysimachia vulgaris', nb: 'Fredløs' },
+        variabel: 'vanlig art',
+      },
+      AR_102818: {
+        kode: 'AR_102818',
+        tittel: { la: 'Lythrum salicaria', nb: 'Kattehale' },
+        variabel: 'vanlig art',
+      },
+      AR_103183: {
+        kode: 'AR_103183',
+        tittel: { la: 'Thalictrum flavum', nb: 'Gul frøstjerne' },
+        variabel: 'vanlig art',
+      },
+      AR_103310: {
+        kode: 'AR_103310',
+        tittel: { la: 'Filipendula ulmaria', nb: 'Mjødurt' },
+        variabel: 'vanlig art',
+      },
+      AR_99722: {
+        kode: 'AR_99722',
+        tittel: { la: 'Carex salina', nb: 'Fjærestarr' },
+        variabel: 'vanlig art',
+      },
+      AR_99789: {
+        kode: 'AR_99789',
+        tittel: { la: 'Schoenoplectus lacustris', nb: 'Sjøsivaks' },
+        variabel: 'vanlig art',
+      },
+    },
+  },
+  sti: 'NA/M/8',
+  tittel: { nb: 'Helofytt-saltvannssump' },
+}
+
 storiesOf('KodeVindu', module)
   .addDecorator(muiTheme())
   .add('default', () => (
     <KodeVindu
       data={api}
       meta={meta}
+      onUpdateLayerProp={action('updateLayerProp')}
+      onMouseEnter={action('mouseEnter')}
+      onMouseLeave={action('mouseLeave')}
+      onClick={action('click')}
+      onFitBounds={action('onFitBounds')}
+      language={['nb', 'la']}
+    />
+  ))
+
+storiesOf('KodeVindu2', module)
+  .addDecorator(muiTheme())
+  .add('default', () => (
+    <KodeVindu
+      data={api2}
+      meta={meta2}
       onUpdateLayerProp={action('updateLayerProp')}
       onMouseEnter={action('mouseEnter')}
       onMouseLeave={action('mouseLeave')}
