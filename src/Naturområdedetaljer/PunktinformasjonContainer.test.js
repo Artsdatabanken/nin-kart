4c3760b0ab4538d959815cd73ba77134f917b87d
Jarle Pedersen <jarle.pedersen@norconsult.com>
2018-04-16 14:34:15 +0200

    remove special handling of top nodes (#279)
    * remove special handling of top nodes
    
    * added tests for punktinformasjonsContainer

--- src/Naturområdedetaljer/PunktinformasjonContainer.test.js
+++ src/Naturområdedetaljer/PunktinformasjonContainer.test.js
@@ -1,6 +1,43 @@
 import PunktinformasjonContainer from './PunktinformasjonContainer'
+import backend from '../backend'
 
 var punktinformasjonContainer = new PunktinformasjonContainer()
+
+var createNatureAreaPointInfoName = 'NA_T32-C-3'
+var createNatureAreaPointInfoValue = 'intermediær eng med mindre hevdpreg'
+
+var naturAreaExpected = {
+  name: createNatureAreaPointInfoName,
+  value: createNatureAreaPointInfoValue,
+  logo:
+    'https://pbs.twimg.com/profile_images/378800000067455227/3d053db6b9593d47a02ced7709846522_400x400.png',
+  homepage: 'http://www.miljodirektoratet.no/',
+  dataorigin: 'MDIR',
+  article: 'https://www.artsdatabanken.no/NiN2.0/T32-C-3',
+}
+
+var getNatureAreaFactsInput = {
+  nivå: 'NA',
+  surveyScale: '1 : 5000',
+  surveyedFrom: 'juleAften',
+  rødlisteKategori: 'LC',
+  parameters: [
+    {
+      code: createNatureAreaPointInfoName,
+      codeDescription: createNatureAreaPointInfoValue,
+    },
+  ],
+  description: 'testdata',
+}
+
+var rødlisteExpected = {
+  name: 'name',
+  value: 'value',
+  logo: backend.getCompanyLogo('ADB'),
+  homepage: 'https://artsdatabanken.no/',
+  dataorigin: 'ADB',
+  article: 'https://www.artsdatabanken.no/rodlistefornaturtyper',
+}
 
 test('fixStedsnavn-NullTest', () => {
   expect(punktinformasjonContainer.fixStedsnavn({ result: 'nada' })).toBe(null)
@@ -17,47 +54,26 @@ test('fixStedsnavn', () => {
   ).toBe('GEO_SN-42')
 })
 
-var createNatureAreaPointInfoName = 'NA_T32-C-3'
-var createNatureAreaPointInfoValue = 'intermediær eng med mindre hevdpreg'
+test('createNatureAreaPointInfo', () => {
+  expect(
+    punktinformasjonContainer.createNatureAreaPointInfo(
+      createNatureAreaPointInfoName,
+      createNatureAreaPointInfoValue
+    ).article
+  ).toBe(naturAreaExpected.article)
+})
 
-var naturAreaExpected = {
-  name: createNatureAreaPointInfoName,
-  value: createNatureAreaPointInfoValue,
-  logo:
-    'https://pbs.twimg.com/profile_images/378800000067455227/3d053db6b9593d47a02ced7709846522_400x400.png',
-  homepage: 'http://www.miljodirektoratet.no/',
-  dataorigin: 'MDIR',
-  article: 'https://www.artsdatabanken.no/Pages/222921',
-}
+test('getNatureAreaFacts', () => {
+  expect(
+    punktinformasjonContainer.getNatureAreaFacts(getNatureAreaFactsInput).length
+  ).toBe(undefined)
+})
 
-test('createNatureAreaPointInfo'),
-  () => {
-    expect(
-      punktinformasjonContainer.createNatureAreaPointInfo(
-        createNatureAreaPointInfoName,
-        createNatureAreaPointInfoValue
-      )
-    ).toBe(naturAreaExpected)
-  }
-
-var getNatureAreaFactsInput = {
-  nivå: 'NA',
-  surveyScale: '1 : 5000',
-  surveyedFrom: 'juleAften',
-  rødlisteKategori: 'LC',
-  parameters: [
-    {
-      code: createNatureAreaPointInfoName,
-      codeDescription: createNatureAreaPointInfoValue,
-    },
-  ],
-  description: 'testdata',
-}
-
-test('getNatureAreaFacts'),
-  () => {
-    expect(
-      punktinformasjonContainer.getNatureAreaFacts(getNatureAreaFactsInput)
-        .length
-    ).toBe(1)
-  }
+test('createRødlistePointInfo', () => {
+  expect(
+    punktinformasjonContainer.createRødlistePointInfo(
+      rødlisteExpected.name,
+      rødlisteExpected.value
+    ).homepage
+  ).toBe(rødlisteExpected.homepage)
+})
