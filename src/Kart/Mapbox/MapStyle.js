0a4045dbe6ed32e1922c45cb113b65ba69619bfa
amundn <amundn@users.noreply.github.com>
2018-04-19 15:49:27 +0200

    bakgrunnskartene virker igjen, la til lyst kart også (#288)

--- src/Kart/Mapbox/MapStyle.js
+++ src/Kart/Mapbox/MapStyle.js
@@ -2,6 +2,7 @@ import { fromJS } from 'immutable'
 import MAP_STYLE from './default.json'
 import MAP_STYLE_DARK from './dark.json'
 import MAP_STYLE_VINTAGE from './vintage.json'
+import MAP_STYLE_LIGHT from './light'
 import MAP_STYLE_SATELLITE from './satellite.json'
 
 export const Kalk = fromJS({
@@ -171,4 +172,5 @@ export const ar50Snoisbre = fromJS({
 export const defaultMapStyle = fromJS(MAP_STYLE)
 export const darkMapStyle = fromJS(MAP_STYLE_DARK)
 export const vintageMapStyle = fromJS(MAP_STYLE_VINTAGE)
+export const lightMapStyle = fromJS(MAP_STYLE_LIGHT)
 export const satelliteStyle = fromJS(MAP_STYLE_SATELLITE)
