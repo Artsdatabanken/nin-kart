e0a6eeb8e53298751db4825cda2363a7d943698d
amned <amund.nedland@norconsult.com>
2018-04-19 14:31:47 +0200

    bugfix hover vistes ikke debounce for performance bugfix textfield value ikke null oppdatert deck.gl/luma.gl

--- src/Kart/Mapbox/MapStyle.js
+++ src/Kart/Mapbox/MapStyle.js
@@ -126,8 +126,9 @@ export const NiNHover = fromJS({
   'source-layer': 'sqrt42',
   filter: ['==', 'localId', ''],
   paint: {
-    'fill-color': 'hsla(251, 58%, 43%, 0.1)',
+    'fill-color': 'hsla(251, 58%, 43%, 0.2)',
     'fill-outline-color': 'hsla(0, 22%, 22%, 0.52)',
+    'fill-pattern': 'shovel',
   },
 })
 
