3deab514c4e9323a8e17b04153dda2b79fe126e5
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-04-27 10:27:41 +0200

    Søkeresultatet viser blanke linjer (#310)
    * Søkeresultatet viser blanke linjer
    
    * Update snapshots

--- src/språk.js
+++ src/språk.js
@@ -1,4 +1,6 @@
 export default function språk(meta) {
   const keys = Object.keys(meta)
-  return meta[keys[0]]
+  if (keys.length === 1) return meta[keys[0]]
+
+  return `${meta.nb} (${meta.la})`
 }
