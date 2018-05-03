bc4171ba54c0ea9f833d3cc3cf63ea3362f46dbb
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-05-04 01:48:17 +0200

    Hvor mange linjer mangler.. før grønnfargen kommer opp?

--- src/AktiveKartlag/index.js
+++ src/AktiveKartlag/index.js
@@ -16,31 +16,32 @@ const AktiveKartlag = ({
   onUpdateLayerProp,
   language,
 }) => {
-  if (!koder) return null
   return (
-    <React.Fragment>
-      <Subheader>Aktive kartlag</Subheader>
-      {koder.map(item => {
-        const kode = item.kode
-        return (
-          <Kartlagelement
-            meta={item}
-            erEkspandert={kode === ekspandertKode}
-            kode={kode}
-            key={'valgt' + kode}
-            onGoToCode={onGoToCode}
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-            onClose={kode => onRemoveSelectedLayer(kode)}
-            visHeleKoden={true}
-            onUpdateLayerProp={onUpdateLayerProp}
-            onShowColorpicker={kode => onShowColorpicker(kode)}
-            showColor={onShowColorpicker}
-            language={language}
-          />
-        )
-      })}
-    </React.Fragment>
+    koder && (
+      <React.Fragment>
+        <Subheader>Aktive kartlag</Subheader>
+        {koder.map(item => {
+          const kode = item.kode
+          return (
+            <Kartlagelement
+              meta={item}
+              erEkspandert={kode === ekspandertKode}
+              kode={kode}
+              key={'valgt' + kode}
+              onGoToCode={onGoToCode}
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+              onClose={kode => onRemoveSelectedLayer(kode)}
+              visHeleKoden={true}
+              onUpdateLayerProp={onUpdateLayerProp}
+              onShowColorpicker={kode => onShowColorpicker(kode)}
+              showColor={onShowColorpicker}
+              language={language}
+            />
+          )
+        })}
+      </React.Fragment>
+    )
   )
 }
 
