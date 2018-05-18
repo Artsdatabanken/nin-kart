e01cf8a89fadc64e47eb672415cf23666307ce4f
amned <amund.nedland@norconsult.com>
2018-05-18 10:41:51 +0200

    litt test

---
+++ src/Grunnkart/Grunnkart.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Grunnkart from './Grunnkart'
+
+test('Grunnkart', () => {
+  const wrapper = shallow(<Grunnkart />)
+  expect(wrapper).toMatchSnapshot()
+  wrapper.setProps({ visValgte: true })
+  expect(wrapper).toMatchSnapshot()
+})
