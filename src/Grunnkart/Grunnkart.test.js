eb5d2870d38b3ba192e0a39f4373f7b7b62998e7
amned <amund.nedland@norconsult.com>
2018-05-18 10:50:23 +0200

    litt test

--- src/Grunnkart/Grunnkart.test.js
+++ src/Grunnkart/Grunnkart.test.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme'
 import Grunnkart from './Grunnkart'
 
 test('Grunnkart', () => {
+  const onQueryChange = jest.fn()
+
   const wrapper = shallow(<Grunnkart />)
   expect(wrapper).toMatchSnapshot()
   wrapper.setProps({ visValgte: true })
