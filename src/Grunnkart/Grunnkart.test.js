f1009de71e8d52b8599db462f72e504a3b7c2115
amned <amund.nedland@norconsult.com>
2018-05-22 13:28:44 +0200

    Remove grunnkart.test

--- src/Grunnkart/Grunnkart.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Grunnkart from './Grunnkart'
-
-test('Grunnkart', () => {
-  const wrapper = shallow(<Grunnkart />)
-  expect(wrapper).toMatchSnapshot()
-})
