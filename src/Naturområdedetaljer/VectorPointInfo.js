610118029346a4485c5afc77a6facda0f03a60a6
Jarle Pedersen <jarle.pedersen@norconsult.com>
2018-05-25 11:58:05 +0200

    List item text order reversed (#381)
    * Reverse order of text in listItems. Strip code from text
    
    * update snapshots

--- src/Naturområdedetaljer/VectorPointInfo.js
+++ src/Naturområdedetaljer/VectorPointInfo.js
@@ -114,7 +114,7 @@ function VectorPointInfo(props) {
               const item = props.pointInfo[key]
               return (
                 <ListItem
-                  primaryText={
+                  secondaryText={
                     item.uom
                       ? Number(item.value).toFixed(1) + ' ' + item.uom
                       : item.value
@@ -124,13 +124,7 @@ function VectorPointInfo(props) {
                     const path = segments.join('/')
                     routing.history.push('/katalog/' + path)
                   }}
-                  secondaryText={
-                    item.name || item.kode
-                      ? props.excludeCode
-                        ? item.name || item.kode
-                        : (item.name || item.kode) + ' (' + key + ')'
-                      : key
-                  }
+                  primaryText={item.name || item.kode}
                   key={key}
                   leftAvatar={
                     item.homepage &&
