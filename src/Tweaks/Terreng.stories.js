7fd9e390d702e4c2d4b072cd6f6b2e7db312d16f
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-05-29 02:31:25 +0200

    Improve test coverage

--- src/Tweaks/Terreng.stories.js
+++ src/Tweaks/Terreng.stories.js
@@ -1,11 +1,11 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 import { muiTheme } from 'storybook-addon-material-ui'
-import Terreng from './Terreng'
-storiesOf('Terreng', module)
+import Tweaks from './'
+storiesOf('Tweaks', module)
   .addDecorator(muiTheme())
   .add('root', () => (
     <div style={{ width: 400 }}>
-      <Terreng />
+      <Tweaks />
     </div>
   ))
