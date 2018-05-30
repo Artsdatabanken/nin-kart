d643dfffc0b2df169e03fd88cbfd779b762ea682
jarle pedersen <jarle.pedersen@norconsult.com>
2018-05-30 10:20:03 +0200

    added seksjoner and soner mbtiles. handle raster-layers as active layer

--- src/Kart/Mapbox/index.js
+++ src/Kart/Mapbox/index.js
@@ -126,6 +126,10 @@ class Mapbox extends Component {
       }
       let aktivtLag = hentLag(map, aktivKode)
       if (aktivtLag) {
+        if (aktivtLag.type === 'raster') {
+          this.addBehindSymbols(map, aktivtLag)
+          return
+        }
         aktivtLag.id = 'aktivt'
 
         aktivtLag.paint['fill-outline-color'] = Color('#ffffff').rgbaString()
@@ -258,7 +262,6 @@ class Mapbox extends Component {
       nextProps.valgteKoder.length > 0
     ) {
       Object.keys(nextProps.valgteKoder).forEach(id => {
-
         const forelder = nextProps.valgteKoder[id]
 
         if (forelder.barn) {
@@ -269,15 +272,15 @@ class Mapbox extends Component {
             if (!item.vis) {
               return
             }
- if (!map.getLayer(lagId)) {
-          let lag = hentLag(map, item.kode)
-          if (!lag || !lag.paint) return
+            if (!map.getLayer(lagId)) {
+              let lag = hentLag(map, item.kode)
+              if (!lag || !lag.paint) return
 
-          let fillColor = Color(item.farge || '#ff2222').alpha(0.7)
-          lag.paint['fill-color'] = fillColor.rgbaString()
-          lag.paint['fill-outline-color'] = Color('#ffffff').rgbaString()
-          lag.id = lagId
-          this.addBehindSymbols(map, lag)
+              let fillColor = Color(item.farge || '#ff2222').alpha(0.7)
+              lag.paint['fill-color'] = fillColor.rgbaString()
+              lag.paint['fill-outline-color'] = Color('#ffffff').rgbaString()
+              lag.id = lagId
+              this.addBehindSymbols(map, lag)
             }
           })
         }
