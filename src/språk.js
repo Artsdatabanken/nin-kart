37c0d9856c92a626a8e8f75e8a31f0eabfab9fa8
amundn <amundn@users.noreply.github.com>
2018-05-30 10:59:56 +0200

    justering på hva som vises fra språk-objektet (#413)
    * justering på hva som vises fra språk-objektet
    
    * ===

--- src/språk.js
+++ src/språk.js
@@ -1,6 +1,13 @@
 export default function språk(meta) {
   const keys = Object.keys(meta)
-  if (keys.length === 1) return meta[keys[0]]
+  if (!keys || keys.length === 0) return ''
 
-  return `${meta.nb} (${meta.la})`
+  if (keys.length === 1) return meta[keys[0]]
+  else if (meta.la) {
+    return `${meta.nb} (${meta.la})`
+  } else if (meta.nb) {
+    return `${meta.nb}`
+  } else {
+    return meta[keys[0]]
+  }
 }
