98bdb8717011a979f55d5fa429663154f7b34bb3
amned <amund.nedland@norconsult.com>
2018-05-31 15:43:57 +0200

    stories

--- src/Tweaks/Tweaks.stories.js
+++ src/Tweaks/Tweaks.stories.js
@@ -2,11 +2,22 @@ import { storiesOf } from '@storybook/react'
 import React from 'react'
 import { muiTheme } from 'storybook-addon-material-ui'
 import Tweaks from './'
+
+const item = {
+  farge: '#666',
+  kode: 'NA',
+  sti: 'na',
+  tittel: 'Natursystem',
+  vis: true,
+  barn: ['ja'],
+  removable: true,
+}
+
 storiesOf('Tweaks', module)
   .addDecorator(muiTheme())
   .add('root', () => (
     <div style={{ width: 400 }}>
       <Tweaks kode={'terreng'} />
-      <Tweaks kode={'NA'} />
+      <Tweaks kode={'NA'} item={item} />
     </div>
   ))
