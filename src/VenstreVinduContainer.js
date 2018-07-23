b15039eb0d657d63924cc978efbcbf900684b0d5
Jarle Pedersen <jarle.pedersen@norconsult.com>
2018-07-23 16:33:25 +0200

    Fix katalog tiny height (#475)
    * Fixes #434
    
    * remove height from div
    
    * remove hardcoded widths
    
    * use hardcoded cutoff to decide width-strategy
    
    * update snapshot
    
    * set height to innerHeight
    
    * updated snapshot
    
    * enable toggling of venstremeny Fixes #474
    
    * updated snapshot

--- src/VenstreVinduContainer.js
+++ src/VenstreVinduContainer.js
@@ -13,7 +13,7 @@ import språk from './språk'
 
 // Alt som dukker opp i vinduet på venstre side av skjermen
 class VenstreVinduContainer extends React.Component {
-  state = { query: '' }
+  state = { query: '', minimized: false }
   queryNumber = 0
 
   handleQueryChange = (e, q) => {
@@ -68,138 +68,156 @@ class VenstreVinduContainer extends React.Component {
 
   render() {
     return (
-      <div
-        style={{
-          backgroundColor: '#fff',
-          position: 'absolute',
-          left: 8,
-          top: 10,
-          paddingBottom: 8,
-          height: window.innerHeight,
-          width: window.innerWidth < 600 ? window.innerWidth - 16 : 392 - 8,
-          zIndex: 2,
-          overflowY: this.state.searchResults === null ? 'auto' : 'initial',
-        }}
-      >
+      <div>
         <Route
           render={({ match, history }) => (
-            <div>
-              {this.state.error && (
-                <Snackbar
-                  open={true}
-                  message={'Søk feilet: ' + JSON.stringify(this.state.error)}
-                  autoHideDuration={4000}
-                  onRequestClose={() => this.setState({ error: null })}
+            <div
+              style={{
+                backgroundColor: '#fff',
+                position: 'absolute',
+                left: 8,
+                top: 10,
+                paddingBottom: 8,
+                height: this.state.minimized ? 48 : window.innerHeight,
+                width:
+                  window.innerWidth < 600 ? window.innerWidth - 16 : 392 - 8,
+                zIndex: 2,
+                overflowY:
+                  this.state.searchResults === null ? 'auto' : 'initial',
+              }}
+            >
+              <div style={{ backgroundColor: '#fff' }}>
+                {this.state.error && (
+                  <Snackbar
+                    open={true}
+                    message={'Søk feilet: ' + JSON.stringify(this.state.error)}
+                    autoHideDuration={4000}
+                    onRequestClose={() => this.setState({ error: null })}
+                  />
+                )}
+                <TopBar
+                  onGoBack={() => history.goBack()}
+                  onExitToRoot={() => {
+                    this.setState({ searchResults: null })
+                    history.push('/')
+                    this.props.onExitToRoot()
+                  }}
+                  onToggleMainDrawer={this.props.onToggleMainDrawer}
+                  isAtRoot={history.location.pathname === '/'}
+                  query={this.state.query}
+                  tittel={this.tittel(this.props.meta)}
+                  parentId={this.state.parentId}
+                  onQueryChange={this.handleQueryChange}
+                  minimized={this.state.minimized}
+                  invertMinimized={minimized => {
+                    if (this.state)
+                      this.setState({ minimized: !this.state.minimized })
+                  }}
                 />
-              )}
-              <TopBar
-                onGoBack={() => history.goBack()}
-                onExitToRoot={() => {
-                  this.setState({ searchResults: null })
-                  history.push('/')
-                  this.props.onExitToRoot()
+              </div>
+              <div
+                style={{
+                  opacity: this.state.minimized ? 0 : 1,
+                  backgroundColor: '#fff',
                 }}
-                onToggleMainDrawer={this.props.onToggleMainDrawer}
-                isAtRoot={history.location.pathname === '/'}
-                query={this.state.query}
-                tittel={this.tittel(this.props.meta)}
-                parentId={this.state.parentId}
-                onQueryChange={this.handleQueryChange}
-              />
-              {this.state.searchResults && (
-                <Paper style={{ position: 'absolute' }} zDepth={2}>
-                  <ResultatListe
-                    query={this.state.query}
-                    searchResults={this.state.searchResults}
-                    language={this.props.language}
-                    onClick={url => {
-                      console.warn('url', url)
-                      this.setState({ query: '', searchResults: null })
-                      history.push('/katalog/' + url)
-                    }}
-                  />
-                </Paper>
-              )}
-              {!this.state.searchResults && (
-                <Switch>
-                  <Route
-                    path="/katalog/:kode*"
-                    render={({ match, history }) => {
-                      return (
-                        <KodeContainer
-                          path={match.params.kode || ''}
-                          onGoToCode={url => {
-                            this.setState({ searchResults: null })
-                            console.log(url)
-                            history.push('/katalog/' + url)
-                          }}
-                          onMouseEnter={this.props.onMouseEnter}
-                          onMouseLeave={this.props.onMouseLeave}
-                          onFitBounds={this.props.onFitBounds}
-                          isActiveLayer={this.props.isActiveLayer}
-                          onToggleLayer={this.props.onToggleLayer}
-                          mapBounds={this.props.mapBounds}
-                          language={this.props.language}
-                          meta={this.props.meta}
-                        />
-                      )
-                    }}
-                  />
-                  <Route
-                    exact
-                    path="/"
-                    render={({ match, history }) => {
-                      return (
-                        <AktiveKartlag
-                          style={{ backgroundColor: '#eee' }}
-                          title="Aktiverte lag"
-                          koder={this.props.valgteKoder}
-                          onMouseEnter={this.props.onMouseEnter}
-                          onMouseLeave={this.props.onMouseLeave}
-                          onToggleVisible={this.props.onToggleVisible}
+              >
+                {this.state.searchResults && (
+                  <Paper style={{ position: 'absolute' }} zDepth={2}>
+                    <ResultatListe
+                      query={this.state.query}
+                      searchResults={this.state.searchResults}
+                      language={this.props.language}
+                      onClick={url => {
+                        console.warn('url', url)
+                        this.setState({ query: '', searchResults: null })
+                        history.push('/katalog/' + url)
+                      }}
+                    />
+                  </Paper>
+                )}
+                {!this.state.searchResults && (
+                  <Switch>
+                    <Route
+                      path="/katalog/:kode*"
+                      render={({ match, history }) => {
+                        return (
+                          <KodeContainer
+                            path={match.params.kode || ''}
+                            onGoToCode={url => {
+                              this.setState({ searchResults: null })
+                              console.log(url)
+                              history.push('/katalog/' + url)
+                            }}
+                            onMouseEnter={this.props.onMouseEnter}
+                            onMouseLeave={this.props.onMouseLeave}
+                            onFitBounds={this.props.onFitBounds}
+                            isActiveLayer={this.props.isActiveLayer}
+                            onToggleLayer={this.props.onToggleLayer}
+                            mapBounds={this.props.mapBounds}
+                            language={this.props.language}
+                            meta={this.props.meta}
+                          />
+                        )
+                      }}
+                    />
+                    <Route
+                      exact
+                      path="/"
+                      render={({ match, history }) => {
+                        return (
+                          <AktiveKartlag
+                            style={{ backgroundColor: '#eee' }}
+                            title="Aktiverte lag"
+                            koder={this.props.valgteKoder}
+                            onMouseEnter={this.props.onMouseEnter}
+                            onMouseLeave={this.props.onMouseLeave}
+                            onToggleVisible={this.props.onToggleVisible}
+                            onRemoveSelectedLayer={
+                              this.props.onRemoveSelectedLayer
+                            }
+                            visKatalog={this.props.visKatalog}
+                          />
+                        )
+                      }}
+                    />
+
+                    <Route
+                      path="/lag/:kode"
+                      render={({ match, history }) => (
+                        <Tweaks
+                          kode={match.params.kode}
                           onRemoveSelectedLayer={
                             this.props.onRemoveSelectedLayer
                           }
-                          visKatalog={this.props.visKatalog}
+                          koder={this.props.valgteKoder}
+                          onGoToCode={sti => {
+                            this.setState({ searchResults: null })
+                            history.push('/katalog/' + sti)
+                          }}
+                          item={this.finnValgtKodeElement(match.params.kode)}
+                          onExitToRoot={() => {
+                            this.setState({ searchResults: null })
+                            history.push('/')
+                            this.props.onExitToRoot()
+                          }}
+                          onFitBounds={this.props.onFitBounds}
+                          updateColor={this.props.updateColor}
                         />
-                      )
-                    }}
-                  />
-
-                  <Route
-                    path="/lag/:kode"
-                    render={({ match, history }) => (
-                      <Tweaks
-                        kode={match.params.kode}
-                        onRemoveSelectedLayer={this.props.onRemoveSelectedLayer}
-                        koder={this.props.valgteKoder}
-                        onGoToCode={sti => {
-                          this.setState({ searchResults: null })
-                          history.push('/katalog/' + sti)
-                        }}
-                        item={this.finnValgtKodeElement(match.params.kode)}
-                        onExitToRoot={() => {
-                          this.setState({ searchResults: null })
-                          history.push('/')
-                          this.props.onExitToRoot()
-                        }}
-                        onFitBounds={this.props.onFitBounds}
-                        updateColor={this.props.updateColor}
-                      />
-                    )}
-                  />
-                  <Route
-                    path="/punkt/:lng,:lat"
-                    render={({ match, history }) => (
-                      <PunktinformasjonContainer
-                        lng={match.params.lng}
-                        lat={match.params.lat}
-                        localId={this.props.localId}
-                      />
-                    )}
-                  />
-                </Switch>
-              )}
+                      )}
+                    />
+                    <Route
+                      path="/punkt/:lng,:lat"
+                      render={({ match, history }) => (
+                        <PunktinformasjonContainer
+                          lng={match.params.lng}
+                          lat={match.params.lat}
+                          localId={this.props.localId}
+                        />
+                      )}
+                    />
+                  </Switch>
+                )}
+              </div>
             </div>
           )}
         />
