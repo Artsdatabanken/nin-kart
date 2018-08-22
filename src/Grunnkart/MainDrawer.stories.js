b38543eeaf6a160ee899a4187046c150adc35b60
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-08-22 12:28:00 +0200

    Update snapshots

--- src/Grunnkart/MainDrawer.stories.js
+++ src/Grunnkart/MainDrawer.stories.js
@@ -2,6 +2,7 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import { action } from '@storybook/addon-actions/dist/index'
 import { storiesOf } from '@storybook/react'
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 import { muiTheme } from 'storybook-addon-material-ui'
 import MainDrawer from './MainDrawer'
 
@@ -9,11 +10,13 @@ storiesOf('MainDrawer', module)
   .addDecorator(muiTheme())
   .add('default', () => (
     <MuiThemeProvider>
-      <div>
-        <MainDrawer
-          open={true}
-          onToggleMainDrawer={action('onToggleMainDrawer')}
-        />
-      </div>
+      <MemoryRouter>
+        <div>
+          <MainDrawer
+            open={true}
+            onToggleMainDrawer={action('onToggleMainDrawer')}
+          />
+        </div>
+      </MemoryRouter>
     </MuiThemeProvider>
   ))
