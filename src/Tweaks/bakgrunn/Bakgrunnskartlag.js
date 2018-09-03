ba374056dad707cb084ad0a3fb36a68e5aa95802
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-09-04 01:19:08 +0200

    Fix warning unused PaintSwatch

--- src/Tweaks/bakgrunn/Bakgrunnskartlag.js
+++ src/Tweaks/bakgrunn/Bakgrunnskartlag.js
@@ -7,7 +7,6 @@ import {
 } from '@material-ui/core'
 import { default as React } from 'react'
 import { withRouter } from 'react-router'
-import PaintSwatch from '../../Kodetre/Kodeliste/PaintSwatch'
 import Checkboard from '../../Kodetre/Kodeliste/Checkboard'
 
 const Bakgrunnskartlag = ({
