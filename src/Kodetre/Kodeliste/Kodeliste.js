895414cba21b1ce8e3e688df71455cde00b4f311
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-09-11 19:29:47 +0200

    Valgfri sortering på kode eller navn

--- src/Kodetre/Kodeliste/Kodeliste.js
+++ src/Kodetre/Kodeliste/Kodeliste.js
@@ -1,6 +1,9 @@
 import { ListSubheader } from '@material-ui/core'
 import React from 'react'
+import { SettingsContext } from '../../SettingsContext'
+import språk from '../../språk'
 import Kodelisteelement from './Kodelisteelement'
+
 const Kodeliste = ({
   title,
   subtitle,
@@ -17,69 +20,65 @@ const Kodeliste = ({
   if (!metadata) return null
   if (Object.keys(metadata) <= 0) return null
   return (
-    <React.Fragment>
-      <ListSubheader>{title}</ListSubheader>
-      {subtitle && (
-        <div
-          style={{
-            padding: '0px 5px 0px 16px',
-            fontSize: '14px',
-            color: 'rgba(95, 95, 95, 0.54)',
-          }}
-        >
-          {subtitle}
-        </div>
+    <SettingsContext.Consumer>
+      {context => (
+        <React.Fragment>
+          <ListSubheader>{title}</ListSubheader>
+          {subtitle && (
+            <div
+              style={{
+                padding: '0px 5px 0px 16px',
+                fontSize: '14px',
+                color: 'rgba(95, 95, 95, 0.54)',
+              }}
+            >
+              {subtitle}
+            </div>
+          )}
+          {Kodeliste.sorterNøkler(metadata, context.sorterPåKode).map(kode => {
+            const apibarn = apidata
+              ? apidata[
+                  apidata
+                    .map(apiItem => {
+                      return apiItem.kode
+                    })
+                    .indexOf(kode.toLowerCase())
+                ] || {}
+              : {}
+            const metabarnet = metadata[kode]
+            return (
+              <Kodelisteelement
+                key={kode}
+                størsteAreal={størsteAreal}
+                {...apibarn}
+                kode={kode}
+                visKode={visKode}
+                meta={metabarnet}
+                onGoToCode={onGoToCode}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+                erOpplyst={kode === opplystKode}
+              />
+            )
+          })}
+        </React.Fragment>
       )}
-      {Kodeliste.sorterNøkler(metadata).map(kode => {
-        const apibarn = apidata
-          ? apidata[
-              apidata
-                .map(apiItem => {
-                  return apiItem.kode
-                })
-                .indexOf(kode.toLowerCase())
-            ] || {}
-          : {}
-        const metabarnet = metadata[kode]
-        return (
-          <Kodelisteelement
-            key={kode}
-            størsteAreal={størsteAreal}
-            {...apibarn}
-            kode={kode}
-            visKode={visKode}
-            meta={metabarnet}
-            onGoToCode={onGoToCode}
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-            erOpplyst={kode === opplystKode}
-          />
-        )
-      })}
-    </React.Fragment>
+    </SettingsContext.Consumer>
   )
 }
 
-const pad = sti => {
-  // t/4 => 0000t0004
-  if (!sti) return ''
-  const e = sti.split(/\//)
-  if (!e) return ''
-  const f = e.map(e => {
-    if (e && e.toString() && e.toString().padStart)
-      return e.toString().padStart(5, '0')
-    return ''
-  })
-  return f.join()
+const nøkkel = (node, sorterPåKode) => {
+  if (node.sortering) return node.sortering
+  if (sorterPåKode) return node.sti.split(/\//).map(e => e.padStart(5, '0'))
+
+  return språk(node.tittel)
 }
 
-Kodeliste.sorterNøkler = metadata => {
-  const sortert = Object.keys(metadata).sort((a, b) => {
-    const ma = metadata[a]
-    const mb = metadata[b]
-    if (ma.sortering && mb.sortering)
-      return ma.sortering > mb.sortering ? 1 : -1
-    return pad(ma.sti) >= pad(mb.sti) ? 1 : -1
+Kodeliste.sorterNøkler = (barn, sorterPåKode) => {
+  const sortert = Object.keys(barn).sort((a, b) => {
+    return nøkkel(barn[a], sorterPåKode) >= nøkkel(barn[b], sorterPåKode)
+      ? 1
+      : -1
   })
   return sortert
 }
