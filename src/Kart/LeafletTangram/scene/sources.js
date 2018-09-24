cc1e87097e7ce23013ccc2748374928437aa5062
Jarle Pedersen <jarle.pedersen@norconsult.com>
2018-09-24 12:21:53 +0200

    read all sources in one go (#696)
    read tiles from hydra

--- src/Kart/LeafletTangram/scene/sources.js
+++ src/Kart/LeafletTangram/scene/sources.js
@@ -1,15 +1,13 @@
 import { isNull } from 'util'
 
-const sources = {}
+var sources = {}
 
 async function lagSource(kode, config) {
   if (!sources.hasOwnProperty(kode) || isNull(sources[kode])) {
-    var fetcher = await fetch(
-      `https://tiles.artsdatabanken.no/data/${kode}.json`
-    )
+    var fetcher = await fetch(`https://nintest.artsdatabanken.no/`)
     if (fetcher.status === 200) {
       var data = await fetcher.json()
-      setSource(data, kode)
+      setSources(data.tilesets)
       setConfig(config, kode)
     }
   } else setConfig(config, kode)
@@ -20,14 +18,18 @@ function setConfig(config, kode) {
   if (sources && config) config.sources[kode] = sources[kode]
 }
 
-function setSource(data, kode) {
-  if (!isNull(data)) {
-    sources[kode] = {
-      type: 'MVT',
-      url: data.tiles[0].replace('http', 'https'),
-      bounds: data.bounds,
-      max_zoom: data.maxzoom,
-      min_zoom: data.minzoom,
+function setSources(tilesets) {
+  if (!isNull(tilesets)) {
+    var keys = Object.keys(tilesets)
+    for (var key in keys) {
+      var kode = keys[key]
+      sources[kode] = {
+        type: 'MVT',
+        url: tilesets[kode].url,
+        bounds: tilesets[kode].bounds,
+        max_zoom: tilesets[kode].zoom[1],
+        min_zoom: tilesets[kode].zoom[0],
+      }
     }
   }
 }
