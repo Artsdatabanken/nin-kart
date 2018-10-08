ef6d497b38c454cf26d38488555366e4811e026f
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-10-08 08:35:51 +0200

    Upgrade to ejected create-react-app v2

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,9 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register'
+registerRequireContextHook()
+
 configure({ adapter: new Adapter() })
 
 //jest.mock('@material-ui/core/Tooltip')
