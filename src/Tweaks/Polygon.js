1a8dc2e909447aa4a867436e18ae7da7369d3f3e
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-10-09 12:54:51 +0200

    Also allow codes to be toggled inside settings

--- src/Tweaks/Polygon.js
+++ src/Tweaks/Polygon.js
@@ -1,3 +1,4 @@
+import { SettingsContext } from '../SettingsContext'
 import typesystem from '@artsdatabanken/typesystem'
 import { List, ListSubheader, withStyles } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
@@ -41,79 +42,86 @@ class Polygon extends Component {
     } = this.props
     const undernivå = this.navnPåUndernivå(kode)
     return (
-      <React.Fragment>
-        <ListSubheader>{tittel}</ListSubheader>
-        <Veksle
-          tittel="Vis etiketter"
-          toggled={visEtiketter}
-          onClick={() => onUpdateLayerProp(kode, 'visEtiketter', !visEtiketter)}
-        />
-        {Object.keys(barn).length > 0 && (
-          <Veksle
-            tittel={'Vis ' + undernivå}
-            toggled={visBarn}
-            onClick={() => onUpdateLayerProp(kode, 'visBarn', !visBarn)}
-          />
-        )}
-        {visBarn ? (
-          <List>
-            <ListSubheader style={{ textTransform: 'capitalize' }}>
-              {undernivå}
-            </ListSubheader>
-            <Barneliste
-              forelderkode={kode}
-              aktivtBarn={lag}
-              barn={barn}
-              onMouseEnter={onMouseEnter}
-              onMouseLeave={onMouseLeave}
-              onUpdateLayerProp={(index, felt, verdi) => {
-                barn[index][felt] = verdi
-                onUpdateLayerProp(kode, 'barn', barn)
-              }}
+      <SettingsContext.Consumer>
+        {context => (
+          <React.Fragment>
+            <ListSubheader>{tittel}</ListSubheader>
+            <Veksle
+              tittel="Vis etiketter"
+              toggled={visEtiketter}
+              onClick={() =>
+                onUpdateLayerProp(kode, 'visEtiketter', !visEtiketter)
+              }
             />
-          </List>
-        ) : (
-          <ColorPicker
-            tittel={'Fyllfarge'}
-            color={farge}
-            onChange={farge => {
-              const rgbString = tinycolor(farge.rgb).toRgbString()
-              onUpdateLayerProp(kode, 'farge', rgbString)
-            }}
-          />
+            {Object.keys(barn).length > 0 && (
+              <Veksle
+                tittel={'Vis ' + undernivå}
+                toggled={visBarn}
+                onClick={() => onUpdateLayerProp(kode, 'visBarn', !visBarn)}
+              />
+            )}
+            {visBarn ? (
+              <List>
+                <ListSubheader style={{ textTransform: 'capitalize' }}>
+                  {undernivå}
+                </ListSubheader>
+                <Barneliste
+                  forelderkode={kode}
+                  visKoder={context.visKoder}
+                  aktivtBarn={lag}
+                  barn={barn}
+                  onMouseEnter={onMouseEnter}
+                  onMouseLeave={onMouseLeave}
+                  onUpdateLayerProp={(index, felt, verdi) => {
+                    barn[index][felt] = verdi
+                    onUpdateLayerProp(kode, 'barn', barn)
+                  }}
+                />
+              </List>
+            ) : (
+              <ColorPicker
+                tittel={'Fyllfarge'}
+                color={farge}
+                onChange={farge => {
+                  const rgbString = tinycolor(farge.rgb).toRgbString()
+                  onUpdateLayerProp(kode, 'farge', rgbString)
+                }}
+              />
+            )}
+            {kanSlettes && (
+              <Button
+                color="primary"
+                onClick={e => {
+                  onRemoveSelectedLayer(kode)
+                }}
+                icon={<ActionDelete />}
+              >
+                Fjern
+              </Button>
+            )}
+            <Button
+              color="primary"
+              onClick={() => {
+                history.push('/katalog/' + kode)
+              }}
+              icon={<ActionInfo />}
+            >
+              Info
+            </Button>
+            {bbox && (
+              <Button
+                color="primary"
+                onClick={() => {
+                  onFitBounds(bbox)
+                }}
+              >
+                <ZoomOutMap className={classes.iconSmall} />
+                Zoom til
+              </Button>
+            )}
+          </React.Fragment>
         )}
-        {kanSlettes && (
-          <Button
-            color="primary"
-            onClick={e => {
-              onRemoveSelectedLayer(kode)
-            }}
-            icon={<ActionDelete />}
-          >
-            Fjern
-          </Button>
-        )}
-        <Button
-          color="primary"
-          onClick={() => {
-            history.push('/katalog/' + kode)
-          }}
-          icon={<ActionInfo />}
-        >
-          Info
-        </Button>
-        {bbox && (
-          <Button
-            color="primary"
-            onClick={() => {
-              onFitBounds(bbox)
-            }}
-          >
-            <ZoomOutMap className={classes.iconSmall} />
-            Zoom til
-          </Button>
-        )}
-      </React.Fragment>
+      </SettingsContext.Consumer>
     )
   }
 
