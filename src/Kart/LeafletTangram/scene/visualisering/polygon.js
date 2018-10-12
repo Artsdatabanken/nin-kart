1b5fc7edd03d5febda93092e4728422436c40edf
Jarle Pedersen <jarle.pedersen@norconsult.com>
2018-10-12 12:50:22 +0200

    also show layers when they are leaf nodes (#779)
    * also show layers when they are leaf nodes
    
    * remove unused variable

--- src/Kart/LeafletTangram/scene/visualisering/polygon.js
+++ src/Kart/LeafletTangram/scene/visualisering/polygon.js
@@ -30,7 +30,7 @@ function drawAll({
     layer[kode] = draw({
       kode: kode,
       forelderkode: kode,
-      farge: farge,
+      farge: barn[kode].farge,
       opplystKode: opplystKode,
       visEtiketter: visEtiketter,
     })
@@ -38,7 +38,7 @@ function drawAll({
 }
 
 function draw(args) {
-  let { kode, forelderkode, farge, opplystKode, visEtiketter } = args
+  let { kode, farge, opplystKode, visEtiketter } = args
   farge = opplystKode === kode ? '#f88' : farge
   const layer = {
     draw: {
@@ -55,7 +55,7 @@ function draw(args) {
       },
     },
   }
-  if (kode !== forelderkode) layer.filter = { code: kode }
+  layer.filter = { code: kode }
   if (kode === opplystKode) {
     const lines = layer.draw.lines
     lines.width = '2px'
