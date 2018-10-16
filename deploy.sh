6d27201c94cee60b5c5455669215cc5d4e1f9b55
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-10-16 12:16:31 +0200

    fix:  repository name must be lowercase (#792)

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ echo $1 $2
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t ratatouille .
 docker images
-docker push Artsdatabanken/ratatouille:latest
+docker push artsdatabanken/ratatouille:latest
