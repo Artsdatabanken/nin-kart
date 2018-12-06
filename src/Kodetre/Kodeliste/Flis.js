2dca4613d8ffc1f96b356177e69928e8ccbfade6
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-12-06 13:18:25 +0100

    Keep the maps visible when clicking on the map

--- src/Kodetre/Kodeliste/Flis.js
+++ src/Kodetre/Kodeliste/Flis.js
@@ -32,9 +32,10 @@ class Flis extends Component {
         </div>
       )
     if (!visKoder) return null
-    return (
-      <div style={{ color: 'rgba(0, 0, 0, 0.4)' }}>{kode.substring(3)} </div>
-    )
+    if (visKoder !== 42)
+      return (
+        <div style={{ color: 'rgba(0, 0, 0, 0.4)' }}>{kode.substring(3)} </div>
+      )
     return (
       <Chip
         style={{
