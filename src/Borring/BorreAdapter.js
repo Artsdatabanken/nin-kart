babdaf01aa6c8c878d27295722bc60580c83e956
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-01-23 23:28:30 +0100

    Klikki kart: Lag seksjoner av LKM og BS1-8 (#976)

--- src/Borring/BorreAdapter.js
+++ src/Borring/BorreAdapter.js
@@ -1,22 +1,44 @@
-import React, { Component } from 'react'
-import BorreVindu from './BorreVindu'
+import React, { Component } from "react";
+import BorreVindu from "./BorreVindu";
 
 class BorreAdapter extends Component {
   hackInnSted(sted, data) {
-    if (!sted) return data
-    if (!data) return data
-    data.AO = data.AO || {}
-    let ao = data.AO
-    ao.sted = sted.placename
-    ao.elevasjon = sted.elevation
-    return data
+    if (!sted) return data;
+    if (!data) return data;
+    data.AO = data.AO || {};
+    let ao = data.AO;
+    ao.sted = sted.placename;
+    ao.elevasjon = sted.elevation;
+    return data;
+  }
+
+  flattenOne(data, current, path) {
+    const key = path.shift();
+    if (!current[key]) return;
+    if (path.length > 0) {
+      this.flattenOne(data, current[key].values, path);
+      return;
+    }
+    const children = current[key].values;
+    delete current[key];
+    Object.entries(children).forEach(e => {
+      const [key, value] = e;
+      data[key] = value;
+    });
+  }
+  flattenData(data) {
+    if (!data) return data;
+    this.flattenOne(data, data, ["NA", "NA-LKM"]);
+    this.flattenOne(data, data, ["NA", "NA-BS"]);
+    return data;
   }
 
   render() {
-    const { data, sted, lat, lng, view } = this.props
-    const borrehull = this.hackInnSted(sted, data)
-    return <BorreVindu lat={lat} lng={lng} view={view} barn={borrehull} />
+    const { data, sted, lat, lng, view } = this.props;
+    const borrehull = this.hackInnSted(sted, data);
+    const flattened = this.flattenData(borrehull);
+    return <BorreVindu lat={lat} lng={lng} view={view} barn={flattened} />;
   }
 }
 
-export default BorreAdapter
+export default BorreAdapter;
--- src/Borring/Borring.js
+++ src/Borring/Borring.js
@@ -1,46 +1,48 @@
-import { List, ListSubheader, withTheme } from '@material-ui/core'
-import React, { Component } from 'react'
-import { withRouter } from 'react-router'
-import { SettingsContext } from '../SettingsContext'
-import Seksjon from './Seksjon'
+import { List, ListSubheader, withTheme } from "@material-ui/core";
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+import { SettingsContext } from "../SettingsContext";
+import Seksjon from "./Seksjon";
 
 class Borring extends Component {
   render() {
-    const { barn } = this.props
-    if (!barn) return null
+    const { barn } = this.props;
+    if (!barn) return null;
     return (
       <List>
         <ListSubheader>Kunnskap</ListSubheader>
         <SettingsContext.Consumer>
           {context => {
-            return ['VV', 'AO', 'LA', 'NA', 'RL'].map(kode => {
-              const node = barn[kode]
-              if (!node) return null
-              if (!node.values) return null
-              return Object.keys(node.values).map(subkode => {
-                return (
-                  <Seksjon
-                    key={subkode}
-                    tittel={node.title}
-                    kode={subkode}
-                    kategori={node.title}
-                    node={node.values[subkode]}
-                    visKoder={context.visKoder}
-                    onClick={() => this.onClick(subkode)}
-                  />
-                )
-              })
-            })
+            return ["VV", "AO", "LA", "NA", "RL"].map(prefix => {
+              return Object.keys(barn)
+                .filter(key => key.indexOf(prefix) === 0)
+                .map(kode => {
+                  const node = barn[kode];
+                  if (!node) return null;
+                  if (!node.values) return null;
+                  return (
+                    <Seksjon
+                      key={kode}
+                      tittel={node.title}
+                      kode={kode}
+                      kategori={node.title}
+                      node={node}
+                      visKoder={context.visKoder}
+                      onClick={() => this.onClick(kode)}
+                    />
+                  );
+                });
+            });
           }}
         </SettingsContext.Consumer>
       </List>
-    )
+    );
   }
 
   onClick = kode => {
-    const { history } = this.props
-    history.push('/katalog/' + kode)
-  }
+    const { history } = this.props;
+    history.push("/katalog/" + kode);
+  };
 }
 
-export default withRouter(withTheme()(Borring))
+export default withRouter(withTheme()(Borring));
