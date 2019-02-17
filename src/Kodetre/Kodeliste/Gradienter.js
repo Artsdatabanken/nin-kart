f89b3c6b06047ec8a89de5b82957caa122461a33
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-02-17 15:30:00 +0100

    Grovt utkast til visualisering av gradienter (WIP)

---
+++ src/Kodetre/Kodeliste/Gradienter.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { ListSubheader } from "@material-ui/core";
+import LinearGauge from "./LinearGauge";
+
+class Gradienter extends Component {
+  render() {
+    const {
+      gradient,
+      onNavigate,
+      onMouseEnter,
+      onMouseLeave,
+      opplystKode
+    } = this.props;
+    if (!gradient) return null;
+    return (
+      <React.Fragment>
+        <ListSubheader>defineres av</ListSubheader>
+        {Object.keys(gradient).map(type => {
+          const gr = gradient[type];
+          return (
+            <div key={type}>
+              <LinearGauge
+                tittel={type}
+                url={gr.url}
+                trinn={gr.trinn}
+                onNavigate={onNavigate}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+                opplystKode={opplystKode}
+                utenFarge={true}
+              />
+            </div>
+          );
+        })}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Gradienter;
---
+++ src/Kodetre/Kodeliste/LinearGauge.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+import tinycolor from "tinycolor2";
+import { ListItem, ListItemText } from "@material-ui/core";
+
+const gaugeHeight = 6;
+
+function farge(erPå, col) {
+  if (erPå) return col;
+  //return "#eee";
+  return tinycolor.mix(col, new tinycolor("#eee"), 80).toHexString();
+}
+
+export default class LinearGauge extends Component {
+  render() {
+    const { tittel, trinn, onNavigate, url } = this.props;
+    const antall = trinn.length;
+    let påFra = 99;
+    const påRange = [];
+    for (let i = 0; i < antall; i++) {
+      const t = trinn[i];
+      t.min = (100 / antall) * i;
+      t.max = (100 / antall) * (i + 1);
+      if (t.på && påFra > i) påFra = i;
+      if (!t.på && påFra < i) {
+        påRange.push([trinn[påFra], trinn[i - 1]]);
+        påFra = 99;
+      }
+    }
+    if (påFra < antall - 1) påRange.push([trinn[påFra], trinn[antall - 1]]);
+    const [min, max] = påRange[0];
+    return (
+      <ListItem button dense onClick={() => onNavigate(url)}>
+        <ListItemText
+          primary={tittel + ": " + min.tittel.nb + " - " + max.tittel.nb}
+          secondary={
+            <svg viewBox="-1 -1 102 7.1">
+              <rect
+                x={0}
+                y={0}
+                width={100}
+                height={gaugeHeight}
+                fill="url(#grad1)"
+                style={{ xfilter: "url(#ag)" }}
+                stroke="rgba(0,0,0,0.67)"
+                strokeWidth={0.25}
+              />
+              {trinn.map(e => (
+                <g key={e.min}>
+                  <rect
+                    x={e.min}
+                    width={e.max - e.min}
+                    y={0}
+                    height={gaugeHeight}
+                    fill={farge(e.på, e.farge)}
+                    stroke="none"
+                  >
+                    <title>{e.tittel.nb}</title>
+                  </rect>
+                  {e.max < 100 && (
+                    <line
+                      x1={e.max}
+                      x2={e.max}
+                      y1={0}
+                      y2={gaugeHeight}
+                      strokeWidth="0.5"
+                      stroke="rgba(0,0,0,0.37)"
+                    />
+                  )}
+                  {false && !e.på && <Cross min={e.min} max={e.max} />}
+                </g>
+              ))}
+              <Shadow
+                x1={min.min + 0.2}
+                y1={0 + 0.2}
+                x2={max.max - 0.4}
+                y2={gaugeHeight - 0.2}
+                color1="#bbb"
+                color2="#888"
+              />
+            </svg>
+          }
+        />
+      </ListItem>
+    );
+  }
+}
+
+const Shadow = ({ x1, x2, y1, y2, color1, color2 }) => {
+  return (
+    <g>
+      <line x1={x1} y1={y1} x2={x1} y2={y2} stroke={color1} strokeWidth="0.5" />
+      <line x1={x1} y1={y1} x2={x2} y2={y1} stroke={color1} strokeWidth="0.5" />
+      <line x1={x2} y1={y1} x2={x2} y2={y2} stroke={color2} strokeWidth="0.5" />
+      <line x1={x1} y1={y2} x2={x2} y2={y2} stroke={color2} strokeWidth="0.5" />
+    </g>
+  );
+};
+
+const Cross = ({
+  min,
+  max,
+  stroke = "rgba(128,128,128,0.3)",
+  strokeWidth = 0.5
+}) => {
+  return (
+    <g>
+      {false && (
+        <line
+          x1={min}
+          y1={0}
+          x2={max}
+          y2={gaugeHeight}
+          stroke={stroke}
+          strokeWidth={strokeWidth}
+        />
+      )}
+      <line
+        x1={min}
+        y1={gaugeHeight}
+        x2={max}
+        y2={0}
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+      />
+    </g>
+  );
+};
