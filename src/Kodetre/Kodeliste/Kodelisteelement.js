ce7f57a3633afab4ca1eadad4c79b255bb0fb567
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-02-19 15:03:43 +0100

    Vis bokstavn på natursystem hovedtyper

--- src/Kodetre/Kodeliste/Kodelisteelement.js
+++ src/Kodetre/Kodeliste/Kodelisteelement.js
@@ -22,6 +22,7 @@ function kodeSuffix(kode, parentkode) {
   let i = 0;
   while (parentkode[i] === kode[i]) i++;
 
+  if ("0123456789".indexOf(kode[i]) >= 0) i -= 1;
   if (kode[i] === "-") i++;
   return kode.substring(i);
 }
