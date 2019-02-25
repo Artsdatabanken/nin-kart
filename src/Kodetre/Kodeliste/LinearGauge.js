4ab071e2105bde5462c162b373cd51f97eb426f6
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-02-25 09:18:43 +0100

    more tests

--- src/Kodetre/Kodeliste/LinearGauge.js
+++ src/Kodetre/Kodeliste/LinearGauge.js
@@ -62,92 +62,101 @@ export default class LinearGauge extends Component {
 
 const Gauge = ({ trinn, range }) => {
   return (
-    <svg viewBox="-2 -1 104 13">
-      <defs>
-        <filter id="f1" x="-2" y="0" width="104" height="10">
-          <feOffset result="offOut" in="SourceGraphic" dx="0.051" dy="0.051" />
-          <feGaussianBlur result="blurOut" in="offOut" stdDeviation="0.5" />
-          <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
-        </filter>
-        <filter id="f2" x="0" y="0" width="102" height="10">
-          <feGaussianBlur
-            result="blurOut"
-            in="SourceGraphic"
-            stdDeviation="0.5"
+    <SettingsContext.Consumer>
+      {context => (
+        <svg viewBox="-2 -1 104 13">
+          <defs>
+            <filter id="f1" x="-2" y="0" width="104" height="10">
+              <feOffset result="offOut" in="SourceGraphic" dx="0.1" dy="0.1" />
+              <feGaussianBlur result="blurOut" in="offOut" stdDeviation="0.4" />
+            </filter>
+            <filter id="f2" x="0" y="0" width="102" height="10">
+              <feGaussianBlur
+                result="blurOut"
+                in="SourceGraphic"
+                stdDeviation="0.5"
+              />
+              <feOffset in="blurOut" dx="0.8" dy="0.8" result="DROPSHADOW" />
+              )}
+            </filter>
+          </defs>
+          <rect
+            x={0}
+            y={0}
+            width={100}
+            height={gaugeHeight}
+            fill="#888"
+            filter="url(#f1)"
+            stroke="rgba(0,0,0,0.67)"
+            strokeWidth={0.25}
           />
-          <feOffset in="blurOut" dx="0.4" dy="0.4" result="DROPSHADOW" />
-        </filter>
-      </defs>
-      <rect
-        x={0}
-        y={0}
-        width={100}
-        height={gaugeHeight}
-        fill="#888"
-        filter="url(#f1)"
-        stroke="rgba(0,0,0,0.67)"
-        strokeWidth={0.25}
-      />
-      {range &&
-        [range].map(([min, max]) => (
-          <React.Fragment key={min.min}>
-            <Shadow
-              key={min}
-              x1={min.min}
-              y1={0}
-              x2={max.max}
-              y2={gaugeHeight}
-              color1="rgba(128,128,128,0.9)"
-            />
-            <Etikett x1={0.5 * (min.min + min.max)} x2={25} />
-            {min !== max && <Etikett x1={0.5 * (max.min + max.max)} x2={75} />}
-          </React.Fragment>
-        ))}
-
-      {trinn.map(e => {
-        const colorOff = farge(false, e.farge);
-        const color = farge(e.på, e.farge);
-        return (
-          <React.Fragment key={e.min}>
-            <rect
-              x={e.min}
-              width={e.max - e.min}
-              y={0}
-              height={gaugeHeight}
-              rx={0}
-              fill={color}
-              stroke="none"
-            >
-              <title>{e.tittel.nb}</title>
-              <xanimate
-                attributeName="fill"
-                dur="3s"
-                begin="mouseover"
-                end="mouseout"
-                values={`${colorOff}; ${e.farge}; ${color}`}
-                keyTimes="0; 0.5; 1"
-                fill="freeze"
-              />
-              <animate
-                attributeName="fill"
-                dur="0.2s"
-                begin="mouseover"
-                values={`${color}; ${e.farge}`}
-                keyTimes="0; 1"
-                fill="freeze"
-              />
-              <animate
-                attributeName="fill"
-                dur="0.6s"
-                begin="mouseout"
-                values={`${e.farge}; ${color}`}
-                keyTimes="0; 1"
-                fill="freeze"
-              />
-            </rect>
-            <SettingsContext.Consumer>
-              {context =>
-                context.visKoder && (
+          {range &&
+            [range].map(([min, max]) => (
+              <React.Fragment key={min.min}>
+                <Shadow
+                  key={min}
+                  x1={min.min}
+                  y1={0}
+                  x2={max.max}
+                  y2={gaugeHeight}
+                  color1="rgba(128,128,128,0.9)"
+                />
+                <Etikett x1={0.5 * (min.min + min.max)} x2={25} />
+                {min !== max && (
+                  <Etikett x1={0.5 * (max.min + max.max)} x2={75} />
+                )}
+              </React.Fragment>
+            ))}
+          {trinn.map(e => {
+            const colorOff = farge(false, e.farge);
+            const color = farge(e.på, e.farge);
+            return (
+              <React.Fragment key={e.min}>
+                <rect
+                  x={e.min}
+                  width={e.max - e.min}
+                  y={0}
+                  height={gaugeHeight}
+                  rx={0}
+                  _fill={color}
+                  stroke="none"
+                >
+                  <title>{e.tittel.nb}</title>
+                  <animate
+                    attributeName="fill"
+                    dur="0.5s"
+                    values={`#fff; ${color}`}
+                    keyTimes="0; 1"
+                    fill="freeze"
+                  />
+                  <animate
+                    attributeName="fill"
+                    dur="0.2s"
+                    begin="mouseover"
+                    values={`${color}; ${e.farge}`}
+                    keyTimes="0; 1"
+                    fill="freeze"
+                  />
+                  <animate
+                    attributeName="fill"
+                    dur="0.6s"
+                    begin="mouseout"
+                    values={`${e.farge}; ${color}`}
+                    keyTimes="0; 1"
+                    fill="freeze"
+                  />
+                </rect>
+                {e.max < 100 && (
+                  <line
+                    x1={e.max}
+                    x2={e.max}
+                    y1={0}
+                    y2={gaugeHeight}
+                    strokeWidth="0.5"
+                    stroke="rgba(0,0,0,0.37)"
+                  />
+                )}
+                {context.visKoder && (
                   <text
                     x={0.5 * (e.min + e.max)}
                     y={0.53 * gaugeHeight}
@@ -159,23 +168,13 @@ const Gauge = ({ trinn, range }) => {
                   >
                     {e.kode[e.kode.length - 1].toLowerCase()}
                   </text>
-                )
-              }
-            </SettingsContext.Consumer>
-            {e.max < 100 && (
-              <line
-                x1={e.max}
-                x2={e.max}
-                y1={0}
-                y2={gaugeHeight}
-                strokeWidth="0.5"
-                stroke="rgba(0,0,0,0.37)"
-              />
-            )}
-          </React.Fragment>
-        );
-      })}
-    </svg>
+                )}
+              </React.Fragment>
+            );
+          })}
+        </svg>
+      )}
+    </SettingsContext.Consumer>
   );
 };
 
@@ -195,24 +194,13 @@ const Etikett = ({ x1, x2 }) => {
       fill="transparent"
       xlinkHref="#left"
     >
-      {false && (
-        <animate
-          attributeName="stroke"
-          dur="5000ms"
-          to="#f06d06"
-          fill="freeze"
-        />
-      )}
       <animate
         attributeName="d"
-        dur="2440ms"
+        dur="440ms"
         repeatCount="1"
-        keyTimes="0;
-                       1"
+        keyTimes="0;                       1"
         calcMode="spline"
         keySplines="0.645, 0.045, 0.355, 1"
-        _values="M 0 7 C 0 12 25 7 25 11;
-      M 15 7 C 15 12 25 7 25 11"
         values={`${start};
         ${end}`}
       />
