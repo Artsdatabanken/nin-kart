272132beb46af3532fe131b7e14352eaff6de2c3
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-05 03:12:24 +0100

    Transition to material-ui styles

--- src/Kodetre/Kodeliste/Tittelblokk.js
+++ src/Kodetre/Kodeliste/Tittelblokk.js
@@ -1,9 +1,10 @@
-import { Avatar, Chip, withStyles } from "@material-ui/core";
+import { Avatar, Chip } from "@material-ui/core";
 import { withTheme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
 
-const styles = {
+const useStyles = makeStyles({
   block: {
     padding: "16px 24px 20px"
   },
@@ -16,8 +17,17 @@ const styles = {
   },
   nivå: {
     textTransform: "capitalize"
+  },
+  chip: {
+    position: "absolute",
+    top: -18,
+    right: 24,
+    boxShadow:
+      "0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)",
+    avatar: { backgroundColor: "blue" },
+    icon: { backgroundColor: "green" }
   }
-};
+});
 
 const Tittelblokk = ({
   onToggleLayer,
@@ -31,47 +41,49 @@ const Tittelblokk = ({
   nivå,
   overordnet,
   onNavigate,
-  classes,
   theme,
   children
 }) => {
+  const classes = useStyles();
   return (
-    <React.Fragment>
-      <div style={{ position: "relative", top: -72, right: -10 }} />
-      <div
-        className={classes.block}
+    <div
+      className={classes.block}
+      style={{
+        position: "relative",
+        backgroundColor: farge || "hsl(16, 0%, 50%)"
+      }}
+    >
+      <Chip
+        className={classes.chip}
         style={{
-          position: "relative",
-          backgroundColor: farge || "hsl(16, 0%, 50%)"
+          backgroundColor: chipFarge
         }}
+        label={kode.slice(3)}
+        clickable={true}
+        avatar={
+          <Avatar
+            style={{
+              backgroundColor: farge
+            }}
+          >
+            {kode.slice(0, 2)}
+          </Avatar>
+        }
+      />
+      <Typography
+        variant="h6"
+        className={classes.h1}
+        style={{ color: kontrastfarge }}
+        gutterBottom
       >
-        <Chip
-          style={{
-            backgroundColor: chipFarge,
-            position: "absolute",
-            top: -18,
-            boxShadow:
-              "0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)"
-          }}
-          label={kode.slice(3)}
-          clickable={true}
-          avatar={<Avatar>{kode.slice(0, 2)}</Avatar>}
-        />
-        <Typography
-          variant="h6"
-          className={classes.h1}
-          style={{ color: kontrastfarge }}
-          gutterBottom
-        >
-          {tittel}
-        </Typography>
-        <Typography style={{ color: kontrastfarge }} className={classes.nivå}>
-          {nivå}
-        </Typography>
-        {children}
-      </div>
-    </React.Fragment>
+        {tittel}
+      </Typography>
+      <Typography style={{ color: kontrastfarge }} className={classes.nivå}>
+        {nivå}
+      </Typography>
+      {children}
+    </div>
   );
 };
 
-export default withStyles(styles)(withTheme()(Tittelblokk));
+export default withTheme()(Tittelblokk);
