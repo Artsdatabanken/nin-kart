fb8243f0ff8f3798b6a46bae95e1d8115d2b00e0
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-13 21:06:56 +0100

    Flytter filer

--- src/Kart/LeafletTangram/scene/visualisering/gradient.js
+++ src/Kart/LeafletTangram/scene/visualisering/gradient.js
@@ -1,5 +1,5 @@
 import sysconfig from "../../../../config";
-import lagGradientrampe from "./palette/gradientrampe";
+import lagGradientrampe from "../../../../palette/gradientrampe";
 
 function drawAll(drawArgs) {
   const layer = {
