bac2234ce5b0004bbfbf35364b0bdb0c40ad1c43
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-16 22:44:22 +0100

    Animate curve (#1141)

--- src/Kodetre/Kodeliste/Kurver.js
+++ src/Kodetre/Kodeliste/Kurver.js
@@ -4,9 +4,6 @@ import { ListSubheader } from "@material-ui/core";
 import språk from "../../språk";
 
 const Kurver = ({ meta, punkt, gradient }) => {
-  console.log(meta);
-  console.log(punkt);
-  console.log(gradient);
   if (!meta || !meta.kart || !meta.kart.format) return null;
   const metaErGradient = meta.kart.format.raster_gradient;
   if (!metaErGradient && !meta.kart.format.raster_ruter) return null;
