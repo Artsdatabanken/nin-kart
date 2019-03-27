66c87cc6a03dfa9d80dcc07af1ab5579f59b9cff
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-27 21:39:53 +0100

    Please build Travis
    Nå trenger jeg at du spiller på lag...

--- src/Tweaks/index.js
+++ src/Tweaks/index.js
@@ -1,34 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import Bakgrunnskart from "./bakgrunn/Bakgrunnskart";
-import Google from "./bakgrunn/Google";
-import Polygon from "./Polygon";
-import Gradient from "./Gradient";
-import Indexed from "./Indexed";
-import Ruter from "./Ruter";
 import Generelt from "./Generelt";
-
-const Seksjon = ({ aktivtFormat, ...props }) => {
-  switch (aktivtFormat) {
-    case "osm_lys":
-    case "osm_mørk":
-      return <Bakgrunnskart {...props} />;
-    case "google_hybrid":
-    case "google_satellite":
-      return <Google {...props} />;
-    case "raster_gradient":
-      return <Gradient {...props} />;
-    case "raster_indexed":
-      return <Indexed {...props} />;
-    case "raster_ruter":
-      return <Ruter {...props} />;
-    case "polygon":
-      return <Polygon {...props} />;
-    default:
-      console.error("Unknown " + aktivtFormat);
-      return null;
-  }
-};
+import Seksjon from "./Seksjon";
 
 const Tweaks = ({ history, ...props }) => {
   if (!props.kart) return null;
