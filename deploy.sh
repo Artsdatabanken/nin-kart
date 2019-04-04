356b4ba342db1f0a0017e72b0cb1688d32ab903c
Even <even.juberg@ntnu.no>
2019-04-04 16:49:58 +0200

    Trying to implement deploy to production.

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,6 @@ docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t artsdatabanken/nin-innsyn:latest .
 docker images
 docker push artsdatabanken/nin-innsyn:latest
+tar -zcf nin-kart-frontend.tar.gz /master
+echo Sending files to prod
+sshpass -p $scp_pass scp -o StrictHostKeyChecking=no nin-kart-frontend.tar.gz $scp_user@158.38.128.51:/tmp
