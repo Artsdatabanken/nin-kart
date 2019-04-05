cf3ede2a659745de5615ee1165b9b4ed92a73d77
Even <even.juberg@ntnu.no>
2019-04-05 13:13:46 +0200

    Adding branch for master in travis.yml and fixing tar.command in deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 #docker build -t artsdatabanken/nin-innsyn:latest .
 #docker images
 #docker push artsdatabanken/nin-innsyn:latest
-tar -zcf nin-kart-frontend.tar.gz /build
+tar --directory=build -zcf bundle.tar.gz .
 echo Sending files to prod
-sshpass -p $scp_pass scp -o StrictHostKeyChecking=no nin-kart-frontend.tar.gz $scp_user@158.38.128.51:/tmp
+sshpass -p $scp_pass scp -o StrictHostKeyChecking=no bundle.tar.gz $scp_user@158.38.128.51:/tmp
