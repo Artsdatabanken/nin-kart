c16c38913e4e980d86d07dcd144842f059c8a48f
Even <even.juberg@ntnu.no>
2019-04-05 08:49:12 +0200

    Trying to add conditional deployment in travis.yml and changed the deployment script.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 #docker build -t artsdatabanken/nin-innsyn:latest .
 #docker images
 #docker push artsdatabanken/nin-innsyn:latest
-tar -zcf nin-kart-frontend.tar.gz /master
+tar -zcf nin-kart-frontend.tar.gz /build
 echo Sending files to prod
 sshpass -p $scp_pass scp -o StrictHostKeyChecking=no nin-kart-frontend.tar.gz $scp_user@158.38.128.51:/tmp
