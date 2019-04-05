721a78bdc962fbddba4822fe37aebe244d564304
Even <even.juberg@ntnu.no>
2019-04-05 15:22:39 +0200

    Tweaked deployment script and completed the flow to publish the branched build in a testfolder.

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@
 echo $1
 tar --directory=build -zcf $1.tar.gz .
 echo Sending files to prod
-sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $1.tar.gz $scp_user@158.38.128.51:/tmp
+sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $1.tar.gz $scp_user@158.38.128.51:/tmp/test
 curl -X POST --data-urlencode "payload={\"channel\": \"$2\", \"username\": \"travis not the band\", \"text\": \"$3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
