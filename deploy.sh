7a9334391e6c4b9e0e4db5f543dcbc5d3a9c57b4
Even <even.juberg@ntnu.no>
2019-04-08 10:17:43 +0200

    Cleaning up.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
-#echo $1 $2
-#docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-#docker build -t artsdatabanken/nin-innsyn:latest .
-#docker images
-#docker push artsdatabanken/nin-innsyn:latest
-echo Zipping files
-echo $1
+echo "Zipping files"
+#echo $1
 tar --directory=build -zcf $1.tar.gz .
-echo Sending files to prod
+echo "Sending files to prod"
 sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $1.tar.gz $scp_user@158.38.128.51:/tmp/test
 curl -X POST --data-urlencode "payload={\"channel\": \"$2\", \"username\": \"travis not the band\", \"text\": \"$3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
