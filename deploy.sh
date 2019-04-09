6795bea64b5393e43133fb6462ce33b3ea15626d
Even <even.juberg@ntnu.no>
2019-04-09 10:27:25 +0200

    Trying to post message to github after deployment.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
-if [ "$1" != "master" ]
+#!/bin/bash
+BRANCH=$1
+if [ "$BRANCH" != "master" ]
 then
 echo "Zipping files"
 #echo $1
-tar --directory=build -zcf $1.tar.gz .
+tar --directory=build -zcf $BRANCH.tar.gz .
 echo "Sending files to prod"
-sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $1.tar.gz $scp_user@158.38.128.51:/tmp/test
-sleep 30
-curl -X POST --data-urlencode "payload={\"channel\": \"$2\", \"username\": \"travis not the band\", \"text\": \"$3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
+sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $BRANCH.tar.gz $scp_user@$scp_dest
+sleep 10
+curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Magic\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 else 
 #docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker login -u="$4" -p="$5"
+docker login -u="$2" -p="$3"
 docker build -t artsdatabanken/nin-innsyn:latest .
 docker images
 docker push artsdatabanken/nin-innsyn:latest
