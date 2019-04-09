3df260b061d85cde9fbf425a46e306cda2aaf054
Even <even.juberg@ntnu.no>
2019-04-09 11:13:41 +0200

    Pushing to branch does not trigger the PR.

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,11 @@ echo "Sending files to prod"
 sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $BRANCH.tar.gz $scp_user@$scp_dest
 sleep 10
 curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
+echo "Whats the value of this:"
+echo $TRAVIS_PULL_REQUEST
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
 then
 echo "Posting to github ..."
-echo $TRAVIS_PULL_REQUEST
 echo $TRAVIS_REPO_SLUG
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Magic\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
