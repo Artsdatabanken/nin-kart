813eb4303fff0726b02beb01bf8bb060358df955
Øyvind Bonesrønning <31729342+runningbones@users.noreply.github.com>
2019-04-10 10:43:22 +0200

    Update index.js

--- src/InfoTab/index.js
+++ src/InfoTab/index.js
@@ -17,17 +17,17 @@ const InfoTab = () => (
     <h2>Hvordan navigere?</h2>
     <h3>Søkefeltet</h3>
     <p>
-      Hvis du vet hva du vil se på, er det uten tvil raskest å benytte seg av
-      søkefeltet. Her kan du skive inn og få forslag basert på eksisterende
+      Hvis du vet hva du vil se på, anbefaler vi å bruke søkefeltet. 
+      Her kan du skive inn og få forslag basert på eksisterende
       undersider i Natur i Norge-systemet.
     </p>
-    <h3>Finn en lokasjon</h3>
+    <h3>Finn en lokalitet</h3>
     <p>
-      For å finne en bestemt lokasjon er det best å navigere seg i selve kartet.
+      For å finne all informasjon kartet inneholder på et bestemt sted er det best å navigere i selve kartet.
       Her kan du velge å zoome inn og ut enten ved hjelp av pluss og
-      minusknappene, bruke fingrene på mobil, eller scrollbar på
+      minusknappene, bruke fingrene på mobil eller scrollbar på
       desktop-løsninger. Om du trykker på kartet setter du en markør på stedet
-      du trykte på, og vil få opp mer informasjon om denne plassen. Om du ønsker
+      du trykte på, og du vil få opp mer informasjon om denne plassen. Ønsker du
       å se samlet informasjon om et område, er det den letteste måten å få dette
       opp på.
     </p>
@@ -35,19 +35,18 @@ const InfoTab = () => (
     <p>
       Inne i informasjonsboksen på pc-enheter eller informasjonsfanen på mobil
       vil du kunne finne et felt kalt hierarki som viser hvor du har havnet i
-      sidesystemet. Alt inne i boksen hierarki er ledd ovenfor ditt nåværende
-      tidspunkt. Under dette hierarkiet vil det være forskjellige bokser med
-      undersider du kan navigere deg videre til. Nøyaktig hvordan denne
-      navigeringen fremstår er for tiden under endring.
+      sidesystemet. Alt inne i boksen hierarki er høyere nivå enn ditt nåværende
+      utvalg. Under dette hierarkiet vil det være forskjellige bokser med
+      undersider du kan navigere deg videre til. 
     </p>
 
-    <h2>Fraværende data</h2>
+    <h2>Fravær av data</h2>
     <p>
-      Vi jobber fortløpende med å få tak i og legge til nye datakilder fra våre
+      Vi jobber fortløpende med tilgjengeliggjøre data fra våre
       samarbeidspartnere, men kan ikke garantere at alt finnes til en hver tid.
-      Vi er avhengige av at våre dataleverandører som stiller med åpne data gir
-      oss fri bruk og tilgang for å kunne vise det frem på best mulig måte. Hvis
-      du savner et datasett, anbefaler vi deg å ta kontakt med dataleverandør.
+      Vi er avhengige av at våre dataleverandører gir
+      oss fri bruk og tilgang. Hvis
+      du savner et datasett, anbefaler vi deg å ta kontakt med Artsdatabanken, eller den enkelte dataeier.
     </p>
   </div>
 );
