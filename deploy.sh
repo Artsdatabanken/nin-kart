b1e63d34746ee1b3676bd5bf6face219afdd071a
Even <even.juberg@ntnu.no>
2019-04-10 14:56:04 +0200

    Fixed deploy.sh and travis.yml, also added serverside support for deployment. Everything should be awesome.

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-BRANCH=$1
-echo $BRANCH
-if [ "$BRANCH" != "master" ]
-then
-echo "Zipping files"
-#echo $1
-tar --directory=build -zcf $BRANCH.tar.gz .
-echo "Sending files to prod"
-sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $BRANCH.tar.gz $scp_user@$scp_dest
-sleep 10
-curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
-echo "Whats the value of this:"
+#Please add variables as environmental variables in Travis settings for the project when using this script.
+#These lines should also be added in the "script block" of travis.yml.
+#- export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+#- echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+#echo $BRANCH
+echo $TRAVIS_REPO_SLUG
 echo $TRAVIS_PULL_REQUEST
+github_message=https://nin.artsdatabanken.no/$BRANCH
+BRANCH=$1
+echo "Making archive..."
+tar -czf $BRANCH.tar.gz public/ deploy-www.js
+echo "Deploying..."
+sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $BRANCH.tar.gz $scp_user@$scp_dest
+#Posting to slack to trigger deployment
+curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
 then
 echo "Posting to github ..."
-echo $TRAVIS_REPO_SLUG
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"Magic\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-fi
-else 
-#docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker login -u="$2" -p="$3"
-docker build -t artsdatabanken/nin-innsyn:latest .
-docker images
-docker push artsdatabanken/nin-innsyn:latest
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"${github_message}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
