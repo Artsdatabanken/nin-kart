6f0c84c044941f2c943c556377fba9ab83843883
helemork <helemork@gmail.com>
2019-04-25 13:18:07 +0200

    flyttet kartlag tab ut i egen komponent og fikk orden på inkludering osv

--- src/Grunnkart/Grunnkart.js
+++ src/Grunnkart/Grunnkart.js
@@ -7,14 +7,9 @@ import språk from "../språk";
 import VenstreVinduContainer from "../VenstreVinduContainer";
 import bakgrunnskarttema from "./bakgrunnskarttema";
 import TopBar from "../TopBar/TopBar";
+import Kartlag from "../Kartlag/Kartlag";
 import MobileNavigation from "../components/MobileNavigation";
 import Kart from "../Kart/LeafletTangram";
-import {
-  Layers,
-  Visibility,
-  Close,
-  KeyboardArrowDown
-} from "@material-ui/icons";
 
 const styles = {
   rot: {
@@ -246,80 +241,7 @@ class Grunnkart extends React.Component {
                     />
                   </div>
                 )}
-                {context.aktivTab === "kartlag" && (
-                  <div className="kartlag sidebar">
-                    <div class="sidebar_title_container sidebar_element">
-                      <h1 class="sidebar_title">Kartlag</h1>
-                    </div>
-                    <div class="sidebar_element">
-                      <h3>Instillinger</h3>
-                      <ul className="kartlag_list">
-                        <li>Bakgrunnsfarger</li>
-                      </ul>
-                    </div>
-
-                    <div class="sidebar_element">
-                      <h3>Mine Kartlag</h3>
-                      <ul className="kartlag_list">
-                        <li>
-                          <Layers className="kartlag_main_icon" />
-                          <span className="kartlag_list_title">
-                            Eksempelkartlag
-                          </span>
-                          <span className="kartlag_list_icon_set">
-                            <KeyboardArrowDown />
-                            <Visibility />
-                            <Close />
-                          </span>
-                        </li>
-
-                        <li>
-                          <Layers className="kartlag_main_icon" />
-                          <span className="kartlag_list_title">
-                            Eksempelkartlag med masse kjempelang tekst for å se
-                            hva som skjer
-                          </span>
-                          <span className="kartlag_list_icon_set">
-                            <KeyboardArrowDown />
-                            <Visibility />
-                            <Close />
-                          </span>
-                        </li>
-
-                        <li>
-                          <Layers className="kartlag_main_icon" />
-                          <span className="kartlag_list_title">
-                            Eksempelkartlag med masse litt ekstra tekst
-                          </span>
-                          <span className="kartlag_list_icon_set">
-                            <KeyboardArrowDown />
-                            <Visibility />
-                            <Close />
-                          </span>
-                        </li>
-
-                        <li>
-                          <Layers className="kartlag_main_icon" />
-                          <span className="kartlag_list_title">
-                            Eksempelkartlag
-                          </span>
-                          <span className="kartlag_list_icon_set">
-                            <KeyboardArrowDown />
-                            <Visibility />
-                            <Close />
-                          </span>
-                        </li>
-                      </ul>
-                    </div>
-
-                    <div class="sidebar_element">
-                      <h3>Historikk</h3>
-                      <ul className="kartlag_list">
-                        <li>kommer her</li>
-                      </ul>
-                    </div>
-                  </div>
-                )}
+                {context.aktivTab === "kartlag" && <Kartlag />}
                 <Kart
                   bounds={this.state.fitBounds}
                   latitude={65.4}
