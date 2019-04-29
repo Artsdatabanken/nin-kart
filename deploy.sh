11982c07e2f332c63e802da56a59caf306f9098f
Myrdrahl <even.juberg@artsdatabanken.no>
2019-04-29 09:54:29 +0200

    Deployment fixing (#1310)
    * Trying to implement deploy to production.
    
    * Trying to add conditional deployment in travis.yml and changed the deployment script.
    
    * Trying to add conditional deployment in travis.yml and changed the deployment script.
    
    * Trying to fix the deployment script.
    
    * Installing sshpass.
    
    * Moving the install of sshpass.
    
    * Adding branch for master in travis.yml and fixing tar.command in deploy.sh.
    
    * Added variables for branches in travis.yml and deploy.sh.
    
    * Tweaked deployment script and completed the flow to publish the branched build in a testfolder.
    
    * Commented after_deploy, to prevent mishaps.
    
    * Cleaning up travis.yml.
    
    * Still trying to fix travis.yml, variable seems to flush.
    
    * Still trying to fix travis.yml, variable seems to flush.
    
    * Travis is not a friend of mine.
    
    * Travis is not a friend of mine v2.
    
    * Travis is not a friend of mine v2.
    
    * Fixing the deployment, maybe.
    
    * Cleaning up.
    
    * Added some intelligence in deployment script, to filter master from other branches.
    
    * Fixing own stupidity.
    
    * Fixing statement.
    
    * Obvious mistakes were made.
    
    * Yes, but no, but yes?
    
    * Added login for docker, improved logic in deploy.sh.
    
    * Trying to post message to github after deployment.
    
    * Added condition to prevent posting to branch instead of PR.
    
    * I cannot post, so let's see what these variables contain.
    
    * Pushing to branch does not trigger the PR.
    
    * Checking value of BRANCH.
    
    * Fixed deploy.sh and travis.yml, also added serverside support for deployment. Everything should be awesome.
    
    * Changed tar command.
    
    * Fixing some stupid mistakes.
    
    * Added verbose taring, since file is not present at deployment.
    
    * Removed verbosity, since 'tis OK.
    
    * Cleaned up deploy.sh a little.
    
    * Commented deploy block.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,23 @@
-if [ "$1" != "master" ]
+#!/bin/bash
+#Please add variables as environmental variables in Travis settings for the project when using this script.
+#These lines should also be added in the "script block" of travis.yml.
+#- export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+#- echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+#echo $BRANCH
+BRANCH=$1
+#echo $BRANCH
+#echo $TRAVIS_REPO_SLUG
+#echo $TRAVIS_PULL_REQUEST
+github_message=https://nin.artsdatabanken.no/$BRANCH
+echo "Making archive..."
+tar --directory=build -zcf $BRANCH.tar.gz .
+echo "Deploying..."
+sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $BRANCH.tar.gz $scp_user@$scp_dest
+#Posting to slack to trigger deployment
+curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
+#Posting message to pull request in GitHub
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
 then
-echo "Zipping files"
-#echo $1
-tar --directory=build -zcf $1.tar.gz .
-echo "Sending files to prod"
-sshpass -p $scp_pass scp -o StrictHostKeyChecking=no $1.tar.gz $scp_user@158.38.128.51:/tmp/test
-sleep 30
-curl -X POST --data-urlencode "payload={\"channel\": \"$2\", \"username\": \"travis not the band\", \"text\": \"$3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
-else
-#docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker login -u="$4" -p="$5"
-docker build -t artsdatabanken/nin-kart-frontend:latest .
-docker images
-docker push artsdatabanken/nin-kart-frontend:latest
+echo "Posting to github ..."
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"${github_message}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
