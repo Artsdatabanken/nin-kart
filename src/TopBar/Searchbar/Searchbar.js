adab2d38622f7cd0bdbe3841eda30ced373f5cd1
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-06-11 13:18:56 +0200

    Fjern ubrukt settingsContext

--- src/TopBar/Searchbar/Searchbar.js
+++ src/TopBar/Searchbar/Searchbar.js
@@ -4,7 +4,6 @@ import Search from "@material-ui/icons/Search";
 import Close from "@material-ui/icons/Close";
 import React, { useState, useRef } from "react";
 import { withRouter } from "react-router-dom";
-import { SettingsContext } from "SettingsContext";
 
 const abc = (cl, isSearching) => classNames(cl, isSearching && "mobile_active");
 
@@ -18,48 +17,44 @@ const Searchbar = ({ query, onQueryChange, hits, setHits }) => {
   useStartTyping(() => inputField.current.focus());
 
   return (
-    <SettingsContext.Consumer>
-      {context => (
-        <div className={abc("searchbar_container", isSearching)}>
-          <input
-            ref={inputField}
-            value={query}
-            placeholder={"Søk i Natur i Norge"}
-            onChange={onQueryChange}
+    <div className={abc("searchbar_container", isSearching)}>
+      <input
+        ref={inputField}
+        value={query}
+        placeholder={"Søk i Natur i Norge"}
+        onChange={onQueryChange}
+      />
+
+      {!isSearching && hits.length === 0 ? (
+        <button
+          onClick={() => {
+            // Logikk for at på mobil skal denne ikke kunne endres før
+            // hits.length endrer seg. men på desktop skal den alltid kunne søke.
+
+            setIsSearching(true);
+
+            // AKTIVER SØK FrA SØKEFELTET om den ikke er aktivert
+          }}
+          className="invisible_icon_button search_icon"
+        >
+          <Search />
+        </button>
+      ) : (
+        <>
+          <div
+            className="background_click_collector"
+            onKeyDown={() => close_dropdown()}
+            onMouseDown={() => close_dropdown()}
           />
-
-          {!isSearching && hits.length === 0 ? (
-            <button
-              onClick={() => {
-                // Logikk for at på mobil skal denne ikke kunne endres før
-                // hits.length endrer seg. men på desktop skal den alltid kunne søke.
-
-                setIsSearching(true);
-
-                // AKTIVER SØK FrA SØKEFELTET om den ikke er aktivert
-              }}
-              className="invisible_icon_button search_icon"
-            >
-              <Search />
-            </button>
-          ) : (
-            <>
-              <div
-                className="background_click_collector"
-                onKeyDown={() => close_dropdown()}
-                onMouseDown={() => close_dropdown()}
-              />
-              <button
-                onClick={() => close_dropdown()}
-                className="invisible_icon_button search_icon"
-              >
-                <Close />
-              </button>
-            </>
-          )}
-        </div>
+          <button
+            onClick={() => close_dropdown()}
+            className="invisible_icon_button search_icon"
+          >
+            <Close />
+          </button>
+        </>
       )}
-    </SettingsContext.Consumer>
+    </div>
   );
 };
 
