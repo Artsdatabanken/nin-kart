1676996bf62d5ac68128467500ea39f4e2b4610d
helemork <helemork@gmail.com>
2019-06-12 15:07:04 +0200

    responsivitet og sånn

--- src/Forside/ForsideInformasjon.js
+++ src/Forside/ForsideInformasjon.js
@@ -21,35 +21,37 @@ const ForsideInformasjon = props => {
             setShowInfo(false);
           }}
         >
-          <div className="frontpage_header">
-            <div className="frontpage_icon">
-              <Layers />{" "}
+          <div className="frontpage_body">
+            <div className="frontpage_header">
+              <div className="frontpage_icon">
+                <Layers />{" "}
+              </div>
+              <div>
+                <h1>NiN-Kart</h1>
+                <h2>Natur i Norge</h2>
+              </div>
             </div>
-            <div>
-              <h1>NiN-Kart</h1>
-              <h2>Natur i Norge</h2>
+
+            <div className="frontpage_feature_block">
+              <h2>Hva er Natur i Norge?</h2>
+              <p>
+                {" "}
+                Natur i Norge (NiN) er et type- og beskrivelsessystem for all
+                variasjon i naturen. NiN håndterer variasjonen i alle naturmiljø
+                i Norge, fra de store havdyp til de høyeste fjell, og fra
+                Skagerrak i sør til Svalbard og Polhavet i nord. Les mer om
+                prosjektet{" "}
+                <a href="https://www.artsdatabanken.no/NiN">
+                  på Artsdatabankens nettsider om NiN.
+                </a>
+              </p>
+            </div>
+
+            <div className="frontpage_link_items">
+              <h2>Utforsk NiN-kart</h2>
+              <Utforsk parent={props} />
             </div>
           </div>
-
-          <div className="frontpage_feature_block">
-            <h2>Hva er Natur i Norge?</h2>
-            <p>
-              {" "}
-              Natur i Norge (NiN) er et type- og beskrivelsessystem for all
-              variasjon i naturen. NiN håndterer variasjonen i alle naturmiljø i
-              Norge, fra de store havdyp til de høyeste fjell, og fra Skagerrak
-              i sør til Svalbard og Polhavet i nord. Les mer om prosjektet{" "}
-              <a href="https://www.artsdatabanken.no/NiN">
-                på Artsdatabankens nettsider om NiN.
-              </a>
-            </p>
-          </div>
-
-          <div className="frontpage_link_items">
-            <h2>Utforsk NiN-kart</h2>
-            <Utforsk parent={props} />
-          </div>
-
           <div className="frontpage_footer">
             <img
               src="https://data.artsdatabanken.no/Datakilde/Artsdatabanken/logo_med_navn_408.png"
