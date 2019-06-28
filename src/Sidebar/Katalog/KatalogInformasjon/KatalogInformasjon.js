7260ce1b4196cdaef350fe8332c1a08bc503e706
helemork <helemork@gmail.com>
2019-06-28 09:41:15 +0200

    fixing #1376 midlertidig mens vi venter på ny versjon

--- src/Sidebar/Katalog/KatalogInformasjon/KatalogInformasjon.js
+++ src/Sidebar/Katalog/KatalogInformasjon/KatalogInformasjon.js
@@ -13,8 +13,8 @@ const KatalogInformasjon = ({ meta, onUpdateLayerProp }) => {
   */
   if (!meta) return null;
   const {
-    kode,
-    depth,
+    //kode,
+    //depth,
     prefiks,
     infoUrl,
     overordnet,
@@ -26,9 +26,9 @@ const KatalogInformasjon = ({ meta, onUpdateLayerProp }) => {
 
   return (
     <>
-      {kode === "NN-LA-TI" && (
+      {/*kode === "NN-LA-TI" && (
         <Detaljeringsgrad onUpdateLayerProp={onUpdateLayerProp} value={depth} />
-      )}
+      )*/}
       <KatalogInformasjonsBoks meta={meta} />
 
       <Ekspander visible={prefiks !== "AO" && !!stats} heading="Statistikk">
