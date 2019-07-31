98b1eba2a40c9a452bdd99c7dbb9d600babfe401
helemork <helemork@gmail.com>
2019-07-31 10:54:41 +0200

    sletter alt av funksjoner fra boring relatert til hack1, 2 og 3, samt andre omskrivingsfunksjoner pga dårlig strukturert api

--- src/Sidebar/Lokalitet/LokalitetFunksjoner/flatten.js
+++
@@ -1,5 +0,0 @@
-export default function flatten(values) {
-  const fn = Object.values(values)[0];
-  const kn = Object.values(fn.values)[0];
-  return { fylke: fn.title, kommune: kn.title };
-}
--- src/Sidebar/Lokalitet/LokalitetFunksjoner/formatElevation.js
+++
@@ -1,5 +0,0 @@
-export default function formatElevation(elevation) {
-  if (!elevation) return "";
-  if (elevation < 0) return -elevation + " muh";
-  return elevation + " moh";
-}
