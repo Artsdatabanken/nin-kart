19518c3d150b16c63f6d1d039485e777aaf82508
helemork <helemork@gmail.com>
2019-09-03 15:07:29 +0200

    fikset pck lock igjen siden den ble litt grumpy, og endret de kondisjonelle statementsene i språk til å kutte ned litt på sammenligningene

--- src/Funksjoner/språk.js
+++ src/Funksjoner/språk.js
@@ -1,34 +1,20 @@
 import {exportableSpraak} from "App";
 
-
-
 export default function språk(meta) {
   let spraak = exportableSpraak.state.spraak;  
-
   if (!meta) return "";
   if (typeof meta === "string") return meta;
   const keys = Object.keys(meta);  
-
   if (!keys || keys.length === 0) return meta;
-
   if (keys.length === 1) return meta[keys[0]];
-
-  //if (meta.la) return `${meta.nb} (${meta.la})`;
-  if (spraak === "la"){    
-    if (!meta.la){
-      return `${meta.nb}`;
-    } else {
+  if (spraak === "nb") {
+    return `${meta.nb}`;
+  }else if (spraak === "la" && meta.la){    
     return `${meta.la}`;
-    }
-  }
-  if (spraak === "en") {    
-    if (!meta.en){
-      return `${meta.nb}`;
-    } else {
-    return `${meta.en}`;
-    }
-  }
-  if (spraak === "nb") return `${meta.nb}`;
-    
+  }else if (spraak === "en" && meta.en) {    
+    return `${meta.en}`;  
+  }else if(meta.nb){
+    return `${meta.nb}`;
+  }    
   return meta[keys[0]];
 }
