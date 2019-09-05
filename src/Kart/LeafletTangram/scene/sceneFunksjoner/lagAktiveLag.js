49057a49f749dd1d247567d00a688f2a06181dec
helemork <helemork@gmail.com>
2019-09-05 14:48:37 +0200

    nå lever ikke lagEttLag mer, den ble spist av lagaktivelag der den hører hjemme

--- src/Kart/LeafletTangram/scene/sceneFunksjoner/lagAktiveLag.js
+++ src/Kart/LeafletTangram/scene/sceneFunksjoner/lagAktiveLag.js
@@ -1,9 +1,12 @@
-import lagEttLag from "./lagEttLag";
+import byggLag from "./byggLag";
 
 function lagAktiveLag(aktive, viserKatalog, opplyst, config) {
-  Object.keys(aktive).forEach(kode =>
-    lagEttLag(aktive[kode], opplyst, viserKatalog, config)
-  );
+  Object.keys(aktive).forEach(kode => {
+    let lag = aktive[kode];
+    if (!(!lag.erSynlig && opplyst.kode !== lag.kode)) {
+      byggLag(lag, opplyst, config, viserKatalog);
+    }
+  });
 }
 
 export default lagAktiveLag;
--- src/Kart/LeafletTangram/scene/sceneFunksjoner/lagEttLag.js
+++
@@ -1,6 +0,0 @@
-import byggLag from "./byggLag";
-
-export default function lagEttLag(lag, opplyst, viserKatalog, config) {
-  if (!lag.erSynlig && opplyst.kode !== lag.kode) return;
-  byggLag(lag, opplyst, config, viserKatalog);
-}
