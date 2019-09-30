b5a11db266e61d676860c64d218616c9ae00aa8b
svetlana <svetlana.naumova@artsdatabanken.no>
2019-09-30 11:18:57 +0200

    Rettet opp feil ved visning av badges på mobil

--- src/InformasjonsVisning/Lokalitet/LokalitetElement/Variabelboks.js
+++ src/InformasjonsVisning/Lokalitet/LokalitetElement/Variabelboks.js
@@ -69,6 +69,7 @@ const Variabelboks = ({ onNavigate, miljøvariabel }) => {
           )}
 
           <button
+            className="infosiden"
             onClick={() => {
               onNavigate(miljøvariabel.url);
             }}
