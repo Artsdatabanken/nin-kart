57fcc6fb09dcca0e538bdc5e571628e6f7184d6a
Terje Olsøybakk <terje.olsoybakk@artsdatabanken.no>
2021-10-21 10:07:36 +0200

    #1732 cleanup

--- src/InformasjonsVisning/Lokalitet/LokalitetElement/Ulkm.js
+++ src/InformasjonsVisning/Lokalitet/LokalitetElement/Ulkm.js
@@ -7,30 +7,14 @@ import {
   ListSubheader,
 } from "@material-ui/core";
 import config from "../../../Funksjoner/config";
-import { getParentUrl } from "../../../AppSettings/AppFunksjoner/fetchMeta";
-
-const group = (ulkm) => {
-  return Object.keys(ulkm).reduce((acc, prefix) => {
-    if (prefix === "undefined") return acc; // koder vi mangler
-    var items = Object.values(ulkm[prefix]);
-    // let items = ulkm[prefix];
-    // items = items.filter((e) => !!e.overordnet);
-    if (items.length > 0) acc[prefix] = items;
-    return acc;
-  }, {});
-};
 
 const Ulkm = (props) => {
   const { ulkm, onNavigate } = props;
   if (!ulkm) return null;
-  // const ulkms = group(ulkm);
   return (
     <>
       <ListSubheader>{språk('uLKM')}</ListSubheader>
       {Object.keys(ulkm).map((key) => {
-        // const vari = ulkm[key].sort((a, b) => (a.ulkmkode > b.ulkmkode ? 1 : -1));
-        // const overordnet = vari[0].overordnet;
-        // const bs = overordnet[overordnet.length - 4];
         return (
           <Blokk key={key} ulkm={ulkm[key].ulkmkode} variabler={ulkm[key]} onNavigate={onNavigate} />
         );
