9afd9178d1dd3147e19bf58a1aa394a24a8d98b3
Helene Engeness Mørk <helene.mork@artsdatabanken.no>
2022-01-27 09:57:34 +0100

    lots of rewrite, but turned out component was called two places and one lacked inputvariable.

--- src/HamburgerMeny/Utforsk/Utforsk.js
+++ src/HamburgerMeny/Utforsk/Utforsk.js
@@ -1,79 +1,79 @@
 import Menyelement from "../Menyelement";
-import React from "react";
+import React, { Component } from "react";
 import { Pets, Landscape } from "@material-ui/icons";
 import Naturvern from "../Naturvern";
 
-const Utforsk = ({ parent, context, handleHovedMeny }) => {
-  /*
-  Ridiculous check here to avoid props collapse when called from different places
-  */
-  let the_props = parent;
-  if (!parent.history) {
-    the_props = parent.props;
-  }
+class Utforsk extends Component {
+  render() {
+    const { parent, context, handleHovedMeny } = this.props;
+    let the_props = parent;
+    if (!parent.history) {
+      the_props = parent.props;
+    }
 
-  return (
-    <>
-      <Menyelement
-        onClick={e => {
-          the_props.history.push("/Natur_i_Norge/Natursystem");
-          handleHovedMeny();
-        }}
-        icon={
-          <img
-            className="meny_ikon_image"
-            src="/logoer/natursystem_ikon.png"
-            alt=""
-          />
-        }
-        primary="Natursystem"
-      />
+    function onElementClick(url) {
+      the_props.history.push(url);
+      handleHovedMeny();
+    }
 
-      <Menyelement
-        onClick={e => {
-          the_props.history.push("/Natur_i_Norge/Landskap");
-          handleHovedMeny();
-        }}
-        icon={<Landscape />}
-        primary="Landskap"
-      />
-
-      <Menyelement
-        onClick={e => {
-          the_props.history.push("/Administrativ_grense");
-          handleHovedMeny();
-        }}
-        icon={
-          <img
-            className="meny_ikon_image"
-            src="/logoer/fylke_ikon.png"
-            alt=""
-          />
-        }
-        primary="Administrative grenser"
-      />
-
-      <Menyelement
-        onClick={e => {
-          the_props.history.push("/Naturvernområde/");
-          handleHovedMeny();
-        }}
-        icon={<Naturvern />}
-        primary="Naturvernområder"
-      />
-
-      {false && (
+    return (
+      <>
         <Menyelement
           onClick={e => {
-            the_props.history.push("/Biota/");
-            handleHovedMeny();
+            onElementClick("/Natur_i_Norge/Natursystem");
           }}
-          icon={<Pets />}
-          primary="Arter"
+          icon={
+            <img
+              className="meny_ikon_image"
+              src="/logoer/natursystem_ikon.png"
+              alt=""
+            />
+          }
+          primary="Natursystem"
         />
-      )}
-    </>
-  );
-};
+
+        <Menyelement
+          onClick={e => {
+            onElementClick("/Natur_i_Norge/Landskap");
+          }}
+          icon={<Landscape />}
+          primary="Landskap"
+        />
+
+        <Menyelement
+          onClick={e => {
+            onElementClick("/Administrativ_grense");
+          }}
+          icon={
+            <img
+              className="meny_ikon_image"
+              src="/logoer/fylke_ikon.png"
+              alt=""
+            />
+          }
+          primary="Administrative grenser"
+        />
+
+        <Menyelement
+          onClick={e => {
+            onElementClick("/Naturvernområde/");
+          }}
+          icon={<Naturvern />}
+          primary="Naturvernområder"
+        />
+
+        {false && (
+          <Menyelement
+            onClick={e => {
+              onElementClick("/Biota/");
+            }}
+            icon={<Pets />}
+            primary="Arter"
+          />
+        )}
+      </>
+    );
+  }
+}
 
 export default Utforsk;
