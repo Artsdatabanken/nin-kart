7982969d7ca822cffad434ba997a73fb65f584ec
Helene Engeness Mørk <helene.mork@artsdatabanken.no>
2022-01-31 10:38:52 +0100

    corrected headerelement in contentbox for landskapstype

--- src/InformasjonsVisning/Landskap.js
+++ src/InformasjonsVisning/Landskap.js
@@ -17,17 +17,6 @@ const Landskap = ({ landskap }) => {
         Landskap
       </h3>
 
-      <Card className="nin-card">
-        <p>
-          En landskapstype er en samling av variasjoner i terreng og landeformer
-          som sammen påvirker et større område. I kartleggingen er den minste
-          graden som måles på en kvadratkilometer. Man kan dermed befinne seg i
-          et isbrelandskap uten å være akkurat på en isbre. Det vil også kunne
-          være noen små variasjoner som ikke bestemmer landskapstypen da de er
-          små og derav ikke dominerende.
-        </p>
-      </Card>
-
       <Landskapstype
         heading1={tittel.nb}
         heading2="Landskapstype"
--- src/InformasjonsVisning/Landskapstype.js
+++ src/InformasjonsVisning/Landskapstype.js
@@ -6,78 +6,108 @@ import {
   ListItemText,
   CardActionArea,
   CardMedia,
-  ListSubheader,
+  ListSubheader
 } from "@material-ui/core";
 import Collapse from "@material-ui/core/Collapse";
 import Avatar from "@material-ui/core/Avatar";
 import config from "../Funksjoner/config";
 import { useHistory } from "react-router-dom";
 import NinCard from "./NinCard";
+import { Card } from "@material-ui/core";
+import clsx from "clsx";
+import IconButton from "@material-ui/core/IconButton";
 
-const useStyles = makeStyles((theme) => ({
+import { ExpandMore } from "@material-ui/icons/";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+const useStyles = makeStyles(theme => ({
   root: {
-    margin: 8,
+    margin: 8
   },
   cardheader: {
-    cursor: "pointer",
+    cursor: "pointer"
   },
   media: {
     height: 0,
-    paddingTop: "56.25%", // 16:9
+    paddingTop: "56.25%" // 16:9
   },
   expand: {
     transform: "rotate(0deg)",
     marginLeft: "auto",
     transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
+      duration: theme.transitions.duration.shortest
+    })
   },
   expandOpen: {
-    transform: "rotate(180deg)",
+    transform: "rotate(180deg)"
   },
   avatar: {},
   related: {
-    cursor: "pointer",
+    cursor: "pointer"
   },
   relatedIcon: {
     transform: "rotate(-140deg)",
     color: "#9e9e9e",
     float: "right",
     position: "relative",
-    top: -2,
-  },
+    top: -2
+  }
 }));
 
 export default function Landskapstype(props) {
   const { heading1, heading2, barn, parenturl, url } = props;
   const classes = useStyles();
   const history = useHistory();
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
-    <NinCard
-      image="Natur_i_Norge/Landskap"
-      heading={heading2}
-      canExpand
-      hasData={true}
-    >
-      {(expanded) => (
-        <>
-          <ListItem button onClick={() => history.push(parenturl)}>
-            <ListItemAvatar>
-              <Avatar>
-                {url && (
-                  <img
-                    alt=""
-                    src={config.foto(url)}
-                    style={{ height: 40, width: 40 }}
-                  />
-                )}
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={heading1} />
-          </ListItem>
+    <div className="subsection">
+      <h4>{heading2}</h4>
+      <div className="sectionbox">
+        <p>
+          En landskapstype er en samling av variasjoner i terreng og landeformer
+          som sammen påvirker et større område. I kartleggingen er den minste
+          graden som måles på en kvadratkilometer. Man kan dermed befinne seg i
+          et isbrelandskap uten å være akkurat på en isbre. Det vil også kunne
+          være noen små variasjoner som ikke bestemmer landskapstypen da de er
+          små og derav ikke dominerende.
+        </p>
+      </div>
 
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <div className="sectionbox">
+        <h5 className="sectionbox-header">
+          <button
+            className="avatarlink"
+            onClick={() => history.push(parenturl)}
+          >
+            <Avatar>
+              {url && (
+                <img
+                  alt=""
+                  src={config.foto(url)}
+                  style={{ height: 40, width: 40 }}
+                />
+              )}
+            </Avatar>
+
+            <ListItemText primary={heading1} />
+          </button>
+          <button
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            {false && <MoreVertIcon />}
+            <ExpandMore />
+          </button>
+        </h5>
+        {expanded && (
+          <div>
             {props.url && (
               <CardActionArea onClick={() => history.push(url)}>
                 <CardMedia
@@ -87,11 +117,13 @@ export default function Landskapstype(props) {
                 />
               </CardActionArea>
             )}
+
             <ListSubheader disableSticky>
               Defineres av landskapsgradienter
             </ListSubheader>
+
             {barn &&
-              Object.values(barn).map((b) => (
+              Object.values(barn).map(b => (
                 <Klg
                   key={b.url}
                   trinn={b.trinn || {}}
@@ -99,15 +131,15 @@ export default function Landskapstype(props) {
                   onClick={() => history.push(b.url)}
                 />
               ))}
-          </Collapse>
-        </>
-      )}
-    </NinCard>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
 const Klg = ({ trinn, url, onClick }) => {
-  const aktiv = trinn.filter((t) => t.på);
+  const aktiv = trinn.filter(t => t.på);
   const min = aktiv[0];
   const max = aktiv[aktiv.length - 1];
   const heading1 =
