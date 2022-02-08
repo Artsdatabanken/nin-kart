f1b9593a7d9d5b7b531f3b8bcd16e361ea8338a7
Helene Engeness Mørk <helene.mork@artsdatabanken.no>
2022-02-08 15:23:39 +0100

    bakgrunnskart selector re-added

--- src/Kartlag/AktiveKartlag/AktivtKartlagElement.js
+++ src/Kartlag/AktiveKartlag/AktivtKartlagElement.js
@@ -1,85 +1,68 @@
-import React from "react";
-//import EkspandertUnderMeny from "./EkspandertMeny/EkspandertUnderMeny";
-//import EkspandertInnhold from "./EkspandertMeny/EkspandertInnhold";
-//import EkspanderingsTopplinje from "./EkspanderingsTopplinje";
+import React, { useState } from "react";
+import TemaMeny from "./EkspandertMeny/Visualisering/TemaMeny/TemaMeny";
 import {
   ListItem,
   ListItemSecondaryAction,
-  ListItemText,
+  ListItemText
 } from "@material-ui/core";
 import { VisibilityOutlined, VisibilityOffOutlined } from "@material-ui/icons";
 import språk from "../../Funksjoner/språk";
 import { useHistory } from "react-router-dom";
 
+import ArrowButton from "../../GjenbruksElement/ArrowButton";
+
 const AktivtKartlagElement = ({ kartlag, onUpdateLayerProp }) => {
+  const [expandedSub, setExpandedSub] = useState(false);
   const history = useHistory();
   if (!kartlag) return null;
-  const kode = kartlag.kode;
+  const { kode, kart } = kartlag;
+  const handleExpandClick = () => {
+    setExpandedSub(!expandedSub);
+  };
   return (
-    <ListItem
-      button
-      onClick={() => {
-        history.push(kartlag.url);
-      }}
-    >
-      <ListItemText primary={språk(kartlag.tittel)} />
-      <ListItemSecondaryAction
-        style={{ cursor: "pointer" }}
-        onClick={(e) => {
-          onUpdateLayerProp(kode, "erSynlig", !kartlag.erSynlig);
-          e.stopPropagation();
+    <>
+      <ListItem
+        button
+        onClick={() => {
+          history.push(kartlag.url);
         }}
       >
-        {kartlag.erSynlig ? (
-          <VisibilityOutlined style={{ color: "#777" }} />
-        ) : (
-          <VisibilityOffOutlined style={{ color: "#777" }} />
-        )}
-      </ListItemSecondaryAction>
-    </ListItem>
-    /*
-            <li draggable>
-        <EkspanderingsTopplinje
-          erLokalitet={erLokalitet}
-          erAktivtLag={erAktivtLag}
-          show_current={show_current}
-          handleShowCurrent={handleShowCurrent}
-          kartlag={kartlag}
-          expanded={expanded}
-          setExpanded={setExpanded}
-          context={context}
-          onUpdateLayerProp={onUpdateLayerProp}
-          is_current_object={is_current_object}
-        />
+        <ListItemText primary={språk(kartlag.tittel)} />
+        <ListItemSecondaryAction style={{ cursor: "pointer" }}>
+          <button
+            className="kartlag_element_buttons"
+            onClick={e => {
+              onUpdateLayerProp(kode, "erSynlig", !kartlag.erSynlig);
+              e.stopPropagation();
+            }}
+          >
+            {kartlag.erSynlig ? (
+              <VisibilityOutlined style={{ color: "#777" }} />
+            ) : (
+              <VisibilityOffOutlined style={{ color: "#777" }} />
+            )}
+          </button>
 
-        {expanded && (
-          <>
-            <EkspandertUnderMeny
-              erLokalitet={erLokalitet}
-              kode={kode}
-              context={context}
-              bbox={bbox}
-              onFitBounds={onFitBounds}
-              kartlag={kartlag}
-              onRemoveSelectedLayer={onRemoveSelectedLayer}
-              is_current_object={is_current_object}
-              onUpdateLayerProp={onUpdateLayerProp}
-              aktivtFormat={aktivtFormat}
-              activateLayerFromHistory={activateLayerFromHistory}
-              navhist={navhist}
-            />
+          <ArrowButton
+            className="kartlag_element_buttons"
+            handleExpandClick={handleExpandClick}
+            expanded={expandedSub}
+          />
+        </ListItemSecondaryAction>
+      </ListItem>
 
-            <EkspandertInnhold
-              kode={kode}
-              aktivtFormat={aktivtFormat}
+      {expandedSub && (
+        <div className="subsection">
+          <h4>Innstillinger</h4>
+          {kode === "bakgrunnskart" && (
+            <TemaMeny
               onUpdateLayerProp={onUpdateLayerProp}
-              kartlag={kartlag}
-              is_current_object={is_current_object}
+              aktivtFormat={kart.aktivtFormat}
             />
-          </>
-        )}
-      </li>
-    */
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
