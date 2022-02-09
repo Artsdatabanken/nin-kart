d057eb5bfe4a0d3e650e32cfb9406d6056908855
Mats Sæter <mats.sater@artsdatabanken.no>
2022-02-09 09:33:13 +0100

    remove 'bakgrunnskart' part form 'Kodelisteelement'

--- src/Kartlag/AktiveKartlag/EkspandertMeny/EkspandertInnhold.js
+++ src/Kartlag/AktiveKartlag/EkspandertMeny/EkspandertInnhold.js
@@ -1,110 +1,68 @@
 import React from "react";
-import BakgrunnInnstillinger from "./Visualisering/BgInnstillinger/BakgrunnInnstillinger";
 import LegendeElementer from "./UnderElementer/LegendeElementer";
-import TemaMeny from "./Visualisering/TemaMeny/TemaMeny";
 import GradientEllerTypeinndelt from "./Visualisering/VisualiseringsType/GradientEllerTypeinndelt";
 import GradientFilter from "./Filtere/GradientFilter.js";
-import FargeVelger from "./FellesElementer/FargeVelger";
 import { Slider } from "@material-ui/core";
 
 const EkspandertInnhold = ({ onUpdateLayerProp, meta }) => {
-  const { kode, aktivtFormat, kart, opacity } = meta;
-  const current = aktivtFormat.aktivtFormat;
-  const currenctActiveFormatNode = aktivtFormat.format[current];
+  const { kode, kart, opacity } = meta;
   const blendmode = meta.blendmode || "multiply";
   let hide_blendmodes = false;
 
   return (
-    <div>
-      {kode === "bakgrunnskart" && (
-        <TemaMeny
-          onUpdateLayerProp={onUpdateLayerProp}
-          aktivtFormat={aktivtFormat}
-        />
+    <div className="_kartlag_sub_bg">
+      <GradientEllerTypeinndelt
+        onUpdateLayerProp={onUpdateLayerProp}
+        where={kode}
+        format={kart.format}
+        aktvtKartlagFormat={kart.aktivtFormat}
+      />
+      {!hide_blendmodes && (
+        <>
+          <div className="submeny_container">
+            <input
+              type="radio"
+              name="blendmode"
+              checked={blendmode === "multiply" && "checked"}
+              onChange={e => {
+                onUpdateLayerProp(kode, "blendmode", "multiply");
+              }}
+            />
+            Multiplisert farge
+            <input
+              type="radio"
+              name="blendmode"
+              checked={blendmode === "translucent" && "checked"}
+              onChange={e => {
+                onUpdateLayerProp(kode, "blendmode", "translucent");
+              }}
+            />
+            Helfarget
+          </div>
+
+          {meta.blendmode === "translucent" && (
+            <div className="submeny_container">
+              Gjennomsiktighet i %
+              <Slider
+                className="slider_element"
+                value={opacity || 1}
+                min={0.01}
+                max={1}
+                onChange={(e, value) =>
+                  onUpdateLayerProp(kode, "opacity", value)
+                }
+              />
+            </div>
+          )}
+        </>
       )}
 
-      <>
-        {kode === "bakgrunnskart" ? (
-          <>
-            {aktivtFormat.aktivtFormat === "google_hybrid" ||
-            aktivtFormat.aktivtFormat === "topo4" ||
-            aktivtFormat.aktivtFormat === "google_satellite" ? (
-              <FargeVelger
-                color={currenctActiveFormatNode.tint}
-                onUpdateLayerProp={onUpdateLayerProp}
-                where={kode}
-                what={"kart.format." + current + ".tint"}
-                title={"Velg fargetone for kartbladet"}
-              />
-            ) : (
-              <BakgrunnInnstillinger
-                onUpdateLayerProp={onUpdateLayerProp}
-                aktivtFormat={aktivtFormat}
-              />
-            )}
-          </>
-        ) : (
-          <div className="_kartlag_sub_bg">
-            <>
-              <GradientEllerTypeinndelt
-                onUpdateLayerProp={onUpdateLayerProp}
-                where={kode}
-                format={kart.format}
-                aktvtKartlagFormat={kart.aktivtFormat}
-              />
-              {!hide_blendmodes && (
-                <>
-                  <div className="submeny_container">
-                    <input
-                      type="radio"
-                      name="blendmode"
-                      checked={blendmode === "multiply" && "checked"}
-                      onChange={e => {
-                        onUpdateLayerProp(kode, "blendmode", "multiply");
-                      }}
-                    />
-                    Multiplisert farge
-                    <input
-                      type="radio"
-                      name="blendmode"
-                      checked={blendmode === "translucent" && "checked"}
-                      onChange={e => {
-                        onUpdateLayerProp(kode, "blendmode", "translucent");
-                      }}
-                    />
-                    Helfarget
-                  </div>
-
-                  {meta.blendmode === "translucent" && (
-                    <div className="submeny_container">
-                      Gjennomsiktighet i %
-                      <Slider
-                        className="slider_element"
-                        value={opacity || 1}
-                        min={0.01}
-                        max={1}
-                        onChange={(e, value) =>
-                          onUpdateLayerProp(kode, "opacity", value)
-                        }
-                      />
-                    </div>
-                  )}
-                </>
-              )}
-
-              <GradientFilter
-                meta={meta}
-                onUpdateLayerProp={onUpdateLayerProp}
-              />
-            </>
-            <LegendeElementer
-              meta={meta}
-              onUpdateLayerProp={onUpdateLayerProp}
-              skjul_meny_tittel={true}
-            />
-          </div>
-        )}
-      </>
+      <GradientFilter meta={meta} onUpdateLayerProp={onUpdateLayerProp} />
+      <LegendeElementer
+        meta={meta}
+        onUpdateLayerProp={onUpdateLayerProp}
+        skjul_meny_tittel={true}
+      />
     </div>
   );
 };
