526eb41ec5964cb44bc180541740cb5a442fbb29
Helene Engeness Mørk <helene.mork@artsdatabanken.no>
2023-12-13 13:53:34 +0100

    cleaned up frontpage to look decent

--- src/Forside/ForsideInformasjon.js
+++ src/Forside/ForsideInformasjon.js
@@ -4,6 +4,7 @@ import "../style/Forside.scss";
 import Kilder from "./Kilder";
 import Utforsk from "../HamburgerMeny/Utforsk/Utforsk";
 import { useNavigate } from "react-router";
+import { ChevronRight} from "@material-ui/icons";
 
 const ForsideInformasjon = ({onToggleHovedMeny}) => {
   const navigate = useNavigate();
@@ -12,33 +13,43 @@ const ForsideInformasjon = ({onToggleHovedMeny}) => {
       <div className="frontpage_body">
         <div
           className="frontpage_header"
-          onClick={e => {
-            e.stopPropagation();
-            navigate("/Natur_i_Norge/");
-          }}
+         
         >
           <div className="frontpage_icon">
             <img
               src="/logoer/small_icon_grey_two.png"
-              className="logo_image_frontpage"
+              className="logo_image_frontpage"  
               alt="artsdatabanken logo"
             />{" "}
           </div>
           <div>
             <h1>NiN-Kart</h1>
             <h2>Natur i Norge</h2>
-          </div>
+            
+          </div><p>Kart-tjeneste for Natur i Norge (NiN)-systemet. <br/>
+          Nåværende kartløsning viser data fra NiN-2.</p>
+          <button  onClick={e => {navigate("/Natur_i_Norge/");}} class="primary">
+              Gå til kart 
+              <ChevronRight />
+            </button>
+        </div><div className="frontpage_link_items">
+          <h2>Utforsk NiN-kart via ikonene nedenfor</h2>
+          <Utforsk onToggleHovedMeny={onToggleHovedMeny} />
         </div>
 
         <div className="frontpage_feature_block">
           <div className="frontpage_feature_container">
-            <img src="/logoer/logo_hvit.svg" alt="" />
+            <img src="/logoer/logo_dark.svg" alt="" />
             <div>
               <h2>Hva er Natur i Norge?</h2>
               <p>
                 Natur i Norge (NiN) er et type- og beskrivelsessystem for
-                naturvariasjon.
-                <br />
+                naturvariasjon som benyttes i kartlegging av landets landskap og naturtyper. 
+                
+              </p>
+                
+              <p>
+                 <span>Les mer på </span>
                 <a
                   href="https://www.artsdatabanken.no/NiN"
                   className="inverted_link"
@@ -50,11 +61,10 @@ const ForsideInformasjon = ({onToggleHovedMeny}) => {
           </div>
         </div>
 
-        <div className="frontpage_link_items">
-          <h2>Utforsk NiN-kart via ikonene nedenfor</h2>
-          <Utforsk onToggleHovedMeny={onToggleHovedMeny} />
-        </div>
+        
       </div>
+      
+      
       <div className="frontpage_footer">
         <div className="frontpage_sources">
           <a
--- src/HamburgerMeny/Utforsk/Utforsk.js
+++ src/HamburgerMeny/Utforsk/Utforsk.js
@@ -1,6 +1,6 @@
-import Menyelement from "../Menyelement";
+
 import React from "react";
-import { Pets, Landscape } from "@material-ui/icons";
+import { Landscape, ChevronRight } from "@material-ui/icons";
 import Naturvern from "../Naturvern";
 import { useNavigate } from "react-router-dom";
 
@@ -13,61 +13,46 @@ const Utforsk = ({onToggleHovedMeny}) => {
   }
 
   return (
-    <>
-      <Menyelement
+    <div className="flexit">
+      <button className="secondary"
         onClick={e => {
           onElementClick("/Natur_i_Norge/Natursystem");
-        }}
-        icon={
+        }}>        
           <img
             className="meny_ikon_image"
             src="/logoer/natursystem_ikon.png"
             alt=""
           />
-        }
-        primary="Natursystem"
-      />
+          Natursystem <ChevronRight/>
+        </button>
 
-      <Menyelement
+        <button className="secondary"
         onClick={e => {
           onElementClick("/Natur_i_Norge/Landskap");
-        }}
-        icon={<Landscape />}
-        primary="Landskap"
-      />
+        }}>
+          <Landscape />Landskap <ChevronRight/>
+        </button>
 
-      <Menyelement
+      <button className="secondary"
         onClick={e => {
           onElementClick("/Administrativ_grense");
-        }}
-        icon={
-          <img
+        }}> 
+        <img
             className="meny_ikon_image"
             src="/logoer/fylke_ikon.png"
             alt=""
-          />
-        }
-        primary="Administrative grenser"
-      />
+          />Administrative grenser <ChevronRight/>
+          </button>
+     
 
-      <Menyelement
+      <button className="secondary"
         onClick={e => {
           onElementClick("/Naturvernområde/");
-        }}
-        icon={<Naturvern />}
-        primary="Naturvernområder"
-      />
-
-      {false && (
-        <Menyelement
-          onClick={e => {
-            onElementClick("/Biota/");
-          }}
-          icon={<Pets />}
-          primary="Arter"
-        />
-      )}
-    </>
+        }}>
+          <Naturvern />Naturvernområder<ChevronRight/>
+        </button>
+      
+    </div>
   );
 }
 
